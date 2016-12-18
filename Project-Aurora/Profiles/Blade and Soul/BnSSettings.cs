2ae9d70750a3c95d7dd9c893aa31fa514e2729f0
DeathAngel <spaceangel.sf1@gmail.com>
2016-12-18 02:17:17 +0100

    Added support for Blade and Soul

---
+++ Project-Aurora/Profiles/Blade and Soul/BnSSettings.cs
@@ -0,0 +1,28 @@
+﻿using Aurora.Settings;
+using System.Collections.Generic;
+
+namespace Aurora.Profiles.Blade_and_Soul
+{
+    public class BnSSettings : ProfileSettings
+    {
+        //General
+        public bool first_time_installed;
+
+        //Effects
+        //// Lighting Areas
+        public List<ColorZone> lighting_areas { get; set; }
+
+        public BnSSettings()
+        {
+            //General
+            isEnabled = true;
+            first_time_installed = false;
+
+            //Effects
+            //// Lighting Areas
+            lighting_areas = new List<ColorZone>() {
+                ///new ColorZone(new Devices.DeviceKeys[] { Devices.DeviceKeys.W, Devices.DeviceKeys.A, Devices.DeviceKeys.S, Devices.DeviceKeys.D, Devices.DeviceKeys.SPACE, Devices.DeviceKeys.Q, Devices.DeviceKeys.E, Devices.DeviceKeys.V, Devices.DeviceKeys.R }, System.Drawing.Color.Orange, "Movement")
+            };
+        }
+    }
+}
