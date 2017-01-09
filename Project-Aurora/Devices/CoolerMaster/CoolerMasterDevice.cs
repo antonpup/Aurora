798a388e6ef1d4348cc9aeef667e58c0dfdd2333
棋山 <376625800@qq.com>
2017-01-09 17:02:11 +0100

    Add Cooler Master Masterkeys Pro L Support

--- Project-Aurora/Devices/CoolerMaster/CoolerMasterDevice.cs
+++ Project-Aurora/Devices/CoolerMaster/CoolerMasterDevice.cs
@@ -167,10 +167,19 @@ namespace Aurora.Devices.CoolerMaster
                             }
                             else
                             {
-                                Global.logger.LogLine("Cooler Master device control could not be initialized", Logging_Level.Error);
+                                CoolerMasterSDK.SetControlDevice(CoolerMasterSDK.DEVICE_INDEX.DEV_MKeys_L_White);
+                                if (CoolerMasterSDK.IsDevicePlug() && CoolerMasterSDK.EnableLedControl(true))
+                                {
+                                    isInitialized = true;
+                                    return true;
+                                }
+                                else
+                                { 
+                                    Global.logger.LogLine("Cooler Master device control could not be initialized", Logging_Level.Error);
 
-                                isInitialized = false;
-                                return false;
+                                    isInitialized = false;
+                                    return false;
+                                }
                             }
                         }
 
