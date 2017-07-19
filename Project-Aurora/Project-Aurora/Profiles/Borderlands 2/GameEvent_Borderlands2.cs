e047ce71caf93ceb540362a753bf4fc2b614ec14
Lukas Affolter <lukas.affolter@live.de>
2017-07-19 11:29:32 +0200

    Borderlands 2: Fixed JSON-File with Pointers

--- Project-Aurora/Project-Aurora/Profiles/Borderlands 2/GameEvent_Borderlands2.cs
+++ Project-Aurora/Project-Aurora/Profiles/Borderlands 2/GameEvent_Borderlands2.cs
@@ -90,15 +90,10 @@ namespace Aurora.Profiles.Borderlands2
             {
                 using (MemoryReader memread = new MemoryReader(process_search[0]))
                 {
-                    //(_game_state as GameState_Borderlands2).Player.maximumHealth = memread.ReadFloat(pointers.Health_maximum.baseAddress, pointers.Health_maximum.pointers);
-                    //(_game_state as GameState_Borderlands2).Player.currentHealth = memread.ReadFloat(pointers.Health_current.baseAddress, pointers.Health_current.pointers);
-                    //(_game_state as GameState_Borderlands2).Player.maximumShield = memread.ReadFloat(pointers.Shield_maximum.baseAddress, pointers.Shield_maximum.pointers);
-                    //(_game_state as GameState_Borderlands2).Player.currentShield = memread.ReadFloat(pointers.Shield_current.baseAddress, pointers.Shield_current.pointers);
-
-                    (_game_state as GameState_Borderlands2).Player.maximumHealth = memread.ReadFloat(0x01F003E8, new int[] { 0x0, 0x6E0, 0x298, 0x3AC, 0x58 } );
-                    (_game_state as GameState_Borderlands2).Player.currentHealth = memread.ReadFloat(0x01F003E8, new int[] { 0x0, 0x6E0, 0x298, 0x3AC, 0x6C } );
-                    (_game_state as GameState_Borderlands2).Player.maximumShield = memread.ReadFloat(0x01F003E8, new int[] { 0x0, 0x6E0, 0x298, 0x3B8, 0x58 } );
-                    (_game_state as GameState_Borderlands2).Player.currentShield = memread.ReadFloat(0x01F003E8, new int[] { 0x0, 0x6E0, 0x298, 0x3B8, 0x6C } );
+                    (_game_state as GameState_Borderlands2).Player.maximumHealth = memread.ReadFloat(pointers.Health_maximum.baseAddress, pointers.Health_maximum.pointers);
+                    (_game_state as GameState_Borderlands2).Player.currentHealth = memread.ReadFloat(pointers.Health_current.baseAddress, pointers.Health_current.pointers);
+                    (_game_state as GameState_Borderlands2).Player.maximumShield = memread.ReadFloat(pointers.Shield_maximum.baseAddress, pointers.Shield_maximum.pointers);
+                    (_game_state as GameState_Borderlands2).Player.currentShield = memread.ReadFloat(pointers.Shield_current.baseAddress, pointers.Shield_current.pointers);
                 }
             }
 
