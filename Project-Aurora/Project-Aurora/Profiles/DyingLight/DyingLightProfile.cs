ad3d5aa0bbdf75ad70cbb9cd0282fea6c29e5983
diogotr7 <diogotr7@gmail.com>
2017-09-11 01:03:09 +0100

    Added support for Dying Light

---
+++ Project-Aurora/Project-Aurora/Profiles/DyingLight/DyingLightProfile.cs
@@ -0,0 +1,50 @@
+﻿using Aurora.Settings;
+using Aurora.Settings.Layers;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Aurora.Profiles.DyingLight
+{
+    public class DyingLightProfile : ApplicationProfile
+    {
+        public DyingLightProfile() : base()
+        {
+            
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+            Layers = new System.Collections.ObjectModel.ObservableCollection<Layer>()
+            {
+                new Layer("Movement", new SolidColorLayerHandler()
+                {
+                    Properties = new LayerHandlerProperties()
+                    {
+                        _PrimaryColor = Color.Orange,
+                        _Sequence = new KeySequence(new Devices.DeviceKeys[] { Devices.DeviceKeys.W, Devices.DeviceKeys.A, Devices.DeviceKeys.S, Devices.DeviceKeys.D, Devices.DeviceKeys.SPACE, Devices.DeviceKeys.LEFT_CONTROL })
+                    }
+                }
+                ),
+                new Layer("Healing", new SolidColorLayerHandler()
+                {
+                    Properties = new LayerHandlerProperties()
+                    {
+                        _PrimaryColor = Color.Green,
+                        _Sequence = new KeySequence(new Devices.DeviceKeys[] { Devices.DeviceKeys.H}),
+                    }
+                }
+                ),
+                new Layer("Flashlight", new SolidColorLayerHandler()
+                {
+                    Properties = new LayerHandlerProperties()
+                    {
+                        _PrimaryColor = Color.White,
+                        _Sequence = new KeySequence(new Devices.DeviceKeys[] { Devices.DeviceKeys.T})
+                    }
+                }
+                )
+            };
+        }
+    }
+}
