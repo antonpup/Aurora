3b7eb51bcbc92f0ee3b2810195efe0f9046c1a4b
Diogo Trindade <diogotr7@gmail.com>
2018-08-29 18:14:40 +0100

    Added Diablo III support and fixed some typos

--- Project-Aurora/Project-Aurora/Profiles/QuakeChampions/QuakeChampionsApplication.cs
+++ Project-Aurora/Project-Aurora/Profiles/QuakeChampions/QuakeChampionsApplication.cs
@@ -12,7 +12,7 @@ namespace Aurora.Profiles.QuakeChampions
     public class QuakeChampions : Application
     {
         public QuakeChampions()
-            : base(new LightEventConfig { Name = "QuakeChampions", ID = "QuakeChampions", ProcessNames = new[] { "QuakeChampions.exe"}, ProfileType = typeof(QuakeChampionsProfile), OverviewControlType = typeof(Control_QuakeChampions), GameStateType = typeof(GameState_Wrapper), Event = new GameEvent_Generic(), IconURI = "Resources/quakechampions_195x195.png" })
+            : base(new LightEventConfig { Name = "Quake Champions", ID = "QuakeChampions", ProcessNames = new[] { "QuakeChampions.exe"}, ProfileType = typeof(QuakeChampionsProfile), OverviewControlType = typeof(Control_QuakeChampions), GameStateType = typeof(GameState_Wrapper), Event = new GameEvent_Generic(), IconURI = "Resources/quakechampions_195x195.png" })
         {
             Config.ExtraAvailableLayers.Add("WrapperLights");
         }
