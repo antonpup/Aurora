2f85bd45ded6d99d2a2d939c5176e7654e047be7
Will Bennion <will.bennion@outlook.com>
2019-01-13 00:49:43 +0000

    Replaced RichTextBox with TextBox. Reduced the amount of GC calls, but means no possibility for syntax highlighting. Added timer showing time since last request.

--- Project-Aurora/Project-Aurora/Settings/Window_GSIHttpDebug.xaml.cs
+++ Project-Aurora/Project-Aurora/Settings/Window_GSIHttpDebug.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Documents;
 
@@ -8,6 +9,10 @@ namespace Aurora.Settings {
     /// A window that logs the latest new game state recieved via HTTP, regardless of the provider.
     /// </summary>
     public partial class Window_GSIHttpDebug : Window {
+
+        private Timer timeDisplayTimer;
+        private DateTime? previousRequestTime = null;
+        private DateTime? lastRequestTime = null;
 
         public Window_GSIHttpDebug() {
             InitializeComponent();
@@ -21,18 +26,29 @@ namespace Aurora.Settings {
             // If a gamestate is already stored by the network listener, display it to the user immediately.
             if (Global.net_listener.CurrentGameState != null)
                 SetJsonText(Global.net_listener.CurrentGameState.json);
+
+            // Start a timer to update the time displays for the request
+            timeDisplayTimer = new Timer(_ => Dispatcher.Invoke(() => {
+                if (lastRequestTime.HasValue)
+                    CurRequestTime.Text = lastRequestTime.ToString() + " (" + (DateTime.Now - lastRequestTime).Value.TotalSeconds.ToString("0.00") + "s ago)";
+            }), null, 0, 50);
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
             // When the window closes, we need to clean up our listener, otherwise it'll keep listening and
             // this window will never get garbage collected.
             Global.net_listener.NewGameState -= Net_listener_NewGameState;
+
+            // Destory the timer to ensure it doesn't keep running and eating memory.
+            timeDisplayTimer.Dispose();
         }
 
         private void Net_listener_NewGameState(Profiles.IGameState gamestate) {
             // This needs to be invoked due to the UI thread being different from the networking thread.
             // Without this, an exception is thrown trying to update the text box.
             Dispatcher.Invoke(() => SetJsonText(gamestate.json));
+            // Also record the time this request came in
+            lastRequestTime = DateTime.Now;
         }
 
         /// <summary>
@@ -40,9 +56,7 @@ namespace Aurora.Settings {
         /// </summary>
         private void SetJsonText(string json) {
             // Pretty-print the JSON (add new lines and indentations)
-            string prettyJson = JToken.Parse(json).ToString(Newtonsoft.Json.Formatting.Indented);
-            BodyPreviewTxt.Document.Blocks.Clear();
-            BodyPreviewTxt.Document.Blocks.Add(new Paragraph(new Run(prettyJson)));
+            BodyPreviewTxt.Text = JToken.Parse(json).ToString(Newtonsoft.Json.Formatting.Indented);
         }
     }
 }
