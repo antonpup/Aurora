e833207ecbfe84c9ddad1648f9217bd3321fff35
Gurjot <gurjotsingh1995@yahoo.com>
2019-02-09 09:51:37 +0530

    First commit

--- Project-Aurora/Project-Aurora/Devices/RGBNet/AbstractRGBNetDevice.cs
+++ Project-Aurora/Project-Aurora/Devices/RGBNet/AbstractRGBNetDevice.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Aurora.Settings;
 using RGB.NET.Brushes;
 using RGB.NET.Core;
+using RGB.NET.Devices.Corsair;
 using RGB.NET.Groups;
 using Color = System.Drawing.Color;
 
@@ -56,7 +57,7 @@ namespace Aurora.Devices.RGBNet
         public bool IsKeyboardConnected() => _deviceProvider.Devices.Any(d => d.DeviceInfo.DeviceType == RGBDeviceType.Keyboard);
 
         public bool IsPeripheralConnected() => _deviceProvider.Devices.Any(d => d.DeviceInfo.DeviceType == RGBDeviceType.Mouse); //TODO DarthAffe 03.02.2019: Which devices are "peripherals"?
-
+        
         public bool Initialize()
         {
             lock (_lock)
@@ -65,13 +66,16 @@ namespace Aurora.Devices.RGBNet
 
                 try
                 {
-                    _surface.LoadDevices(_deviceProvider, throwExceptions: true,exclusiveAccessIfPossible: true);
+                    _surface.LoadDevices(_deviceProvider, throwExceptions: true,exclusiveAccessIfPossible: false);
                     _ledGroup?.Detach(); //DarthAffe 03.02.2019: This should never run, but safety first
                     _ledGroup = new ListLedGroup(_deviceProvider.Devices.SelectMany(d => d)) { Brush = _brush };
-                    ILedGroup ledGroup = new ListLedGroup(_surface.Leds);
-                    RGB.NET.Core.Color c = new RGB.NET.Core.Color(27, 184, 235);
-                    ledGroup.Brush = new SolidColorBrush(c);
-                    _surface.Update();
+                    //rgbDevice  = new CorsairDeviceProvider();
+                   // _deviceProvider.Initialize(RGBDeviceType.Fan|RGBDeviceType.Cooler|RGBDeviceType.HeadsetStand);
+                   
+                    
+                    //RGB.NET.Core.Color c = new RGB.NET.Core.Color(27, 184, 235);
+                    //ledGroup.Brush = new SolidColorBrush(c);
+                    //_surface.Update();
                     return IsInitialized();
                 }
                 catch (Exception ex)
@@ -90,6 +94,15 @@ namespace Aurora.Devices.RGBNet
             if (e.Cancel) return false;
 
             _brush.KeyColors = keyColors;
+
+            if (keyColors.ContainsKey(DeviceKeys.Peripheral_Logo))
+            {
+                if (_deviceProvider != null) {
+                    var device = from d in _deviceProvider.Devices where d.DeviceInfo.DeviceType == RGBDeviceType.HeadsetStand select d;
+                ILedGroup ledGroup = new ListLedGroup(device.First());
+                ledGroup.Brush = new SolidColorBrush(new RGB.NET.Core.Color(234, 14, 214));
+                }
+            }
 
             _surface.Update();
             return true;
