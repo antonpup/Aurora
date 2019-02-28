459f4d17580e016316162454b56b2cea119dca38
drhead <runemaste644@gmail.com>
2019-02-28 13:40:37 -0500

    roccat is done

--- Project-Aurora/Project-Aurora/Devices/DeviceManager.cs
+++ Project-Aurora/Project-Aurora/Devices/DeviceManager.cs
@@ -106,9 +106,9 @@ namespace Aurora.Devices
             devices.Add(new DeviceContainer(new Devices.CoolerMaster.CoolerMasterDevice())); // CoolerMaster Device
             devices.Add(new DeviceContainer(new Devices.Corsair.CorsairDevice()));           // Corsair Device
             devices.Add(new DeviceContainer(new Devices.Drevo.DrevoDevice()));               // Drevo Device
+            devices.Add(new DeviceContainer(new Devices.Roccat.RoccatDevice()));             // Roccat Device
 
             /*
-            devices.Add(new DeviceContainer(new Devices.Roccat.RoccatDevice()));             // Roccat Device
             devices.Add(new DeviceContainer(new Devices.Clevo.ClevoDevice()));               // Clevo Device
             devices.Add(new DeviceContainer(new Devices.AtmoOrbDevice.AtmoOrbDevice()));     // AtmoOrb Ambilight Device
             devices.Add(new DeviceContainer(new Devices.UnifiedHID.UnifiedHIDDevice()));     // UnifiedHID Device
