7636f0e72e040b45ca7553e8af840afecbd01ffa
gitmacer <timfirefox@gmail.com>
2019-04-07 18:29:30 +0200

    Add Paused Gamestate variable

--- Project-Aurora/Project-Aurora/Profiles/Subnautica/GSI/Nodes/GameStateNode.cs
+++ Project-Aurora/Project-Aurora/Profiles/Subnautica/GSI/Nodes/GameStateNode.cs
@@ -8,21 +8,28 @@ namespace Aurora.Profiles.Subnautica.GSI.Nodes {
     public class GameStateNode : Node<GameStateNode> {
 
         public int GameState;
+
         /*
         0 = Menu
         1 = Loading
         2 = Playing
         */
-        public bool InGame;
         public bool InMenu;
         public bool loading;
+        public bool Playing;
+        public bool Paused;
+
+        public bool InGame;
 
         internal GameStateNode(string json) : base(json) {
 
             GameState = GetInt("game_state");
-            InGame = GameState == 2;
             InMenu = GameState == 0;
             loading = GameState == 1;
+            Playing = GameState == 2;
+            Paused = GameState == 3;
+
+            InGame = Playing || Paused;
         }
     }
 }
