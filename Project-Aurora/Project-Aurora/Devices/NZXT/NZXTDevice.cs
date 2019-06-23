aae3aa65d02e072a7248eb072df986756efda248
Diogo Trindade <diogotr7@gmail.com>
2019-06-23 23:19:12 +0100

    Added a layout map instead of relying on the order of the keys being consistent

--- Project-Aurora/Project-Aurora/Devices/NZXT/NZXTDevice.cs
+++ Project-Aurora/Project-Aurora/Devices/NZXT/NZXTDevice.cs
@@ -12,15 +12,46 @@ using Color = System.Drawing.Color;
 
 namespace Aurora.Devices.NZXT
 {
+    public class NZXTDeviceData
+    {
+        public byte[] Colors { get; private set; }
+        public bool Changed { get; private set; }
+
+        public NZXTDeviceData(int leds)
+        {
+            Colors = new byte[leds * 3];
+            Changed = true;
+        }
+
+        public void SetColor(int index, Color clr)
+        {
+            if (index % 3 != 0 || index < 0 || index > Colors.Length - 2)
+                throw new ArgumentOutOfRangeException();
+
+            UpdateValueAt(index, clr.G);
+            UpdateValueAt(index + 1,clr.R);
+            UpdateValueAt(index + 2,clr.B);
+        }
+
+        private void UpdateValueAt(int index, byte value)
+        {
+            if(Colors[index] != value)
+            {
+                Changed = true;
+                Colors[index] = value;
+            }
+        }
+    }
+
     class NZXTDevice : Device
     {
         private String devicename = "NZXT";
         private bool isInitialized = false;
 
         private DeviceLoader DeviceLoader;
-        private byte[] hueDevice = new byte[120];
-        private byte[] krakenringDevice = new byte[24];
-        private Color krakenlogoDevice = Color.Black;
+        private NZXTDeviceData kraken = new NZXTDeviceData(8);
+        private NZXTDeviceData hueplus = new NZXTDeviceData(40);
+        private NZXTDeviceData logo = new NZXTDeviceData(1);
 
         private readonly object action_lock = new object();
 
@@ -124,54 +155,39 @@ namespace Aurora.Devices.NZXT
 
         public bool UpdateDevice(Dictionary<DeviceKeys, Color> keycolors, DoWorkEventArgs e, bool forced = false)
         {
-            int hueIndex = 0, krakenIndex = 0;
-            bool hueChanged = false, krakenChanged = false, krakenLogoChanged = false;
-
             if (e.Cancel) return false;
 
             try
             {
                 foreach (KeyValuePair<DeviceKeys, Color> key in keycolors)
                 {
+                    int index;
                     if (e.Cancel) return false;
+
                     if (key.Key == DeviceKeys.Peripheral_Logo)
                     {
-                        if (krakenlogoDevice != key.Value)
-                        {
-                            krakenlogoDevice = key.Value;
-                            krakenLogoChanged = true;
-                        }
+                        logo.SetColor(0, key.Value);
                     }
-                    else if (key.Key >= DeviceKeys.ONE && key.Key <= DeviceKeys.EIGHT)
-                    {
-                        hueChanged |= UpdateValueAt(hueDevice, hueIndex++, key.Value.G);
-                        hueChanged |= UpdateValueAt(hueDevice, hueIndex++, key.Value.R);
-                        hueChanged |= UpdateValueAt(hueDevice, hueIndex++, key.Value.B);
 
-                        krakenChanged |= UpdateValueAt(krakenringDevice, krakenIndex++, key.Value.G);
-                        krakenChanged |= UpdateValueAt(krakenringDevice, krakenIndex++, key.Value.R);
-                        krakenChanged |= UpdateValueAt(krakenringDevice, krakenIndex++, key.Value.B);
-                    }
-                    else if (key.Key >= DeviceKeys.NINE && key.Key <= DeviceKeys.ZERO      ||
-                             key.Key >= DeviceKeys.Q    && key.Key <= DeviceKeys.P         ||
-                             key.Key >= DeviceKeys.A    && key.Key <= DeviceKeys.SEMICOLON ||
-                             key.Key >= DeviceKeys.Z    && key.Key <= DeviceKeys.FORWARD_SLASH)//40 keys
+                    if((index = GetLedIndex(NZXTLayoutMap.HuePlus, key.Key)) != -1)
                     {
-                        hueChanged |= UpdateValueAt(hueDevice, hueIndex++, key.Value.G);
-                        hueChanged |= UpdateValueAt(hueDevice, hueIndex++, key.Value.R);
-                        hueChanged |= UpdateValueAt(hueDevice, hueIndex++, key.Value.B);
+                        hueplus.SetColor(index, key.Value);
+                    }
+
+                    if ((index = GetLedIndex(NZXTLayoutMap.KrakenX, key.Key)) != -1)
+                    {
+                        kraken.SetColor(index, key.Value);
                     }
                 }
 
-                if (hueChanged || forced)
-                    DeviceLoader.HuePlus?.ApplyEffect(DeviceLoader.HuePlus.Both, new NZXTSharp.Fixed(hueDevice));
+                if (hueplus.Changed || forced)
+                    DeviceLoader.HuePlus?.ApplyEffect(DeviceLoader.HuePlus.Both, new NZXTSharp.Fixed(hueplus.Colors));
 
-                if (krakenChanged || forced)
-                    DeviceLoader.KrakenX?.ApplyEffect(DeviceLoader.KrakenX.Ring, new NZXTSharp.Fixed(krakenringDevice));
+                if (kraken.Changed || forced)
+                    DeviceLoader.KrakenX?.ApplyEffect(DeviceLoader.KrakenX.Ring, new NZXTSharp.Fixed(kraken.Colors));
 
-                if (krakenLogoChanged || forced)
-                    DeviceLoader.KrakenX?.ApplyEffect(DeviceLoader.KrakenX.Logo, new NZXTSharp.Fixed(new NZXTSharp.Color(krakenlogoDevice.R, krakenlogoDevice.G, krakenlogoDevice.B)));
-
+                if (logo.Changed || forced)
+                    DeviceLoader.KrakenX?.ApplyEffect(DeviceLoader.KrakenX.Logo, new NZXTSharp.Fixed(logo.Colors));
 
                 return true;
             }
@@ -214,11 +230,12 @@ namespace Aurora.Devices.NZXT
             return new VariableRegistry();
         }
 
-        private bool UpdateValueAt(byte[] array, int index, byte value)
+        private int GetLedIndex(Dictionary<DeviceKeys, int> layout, DeviceKeys key)
         {
-            var changed = array[index] != value;
-            if (changed) array[index] = value;
-            return changed;
+            if (layout.ContainsKey(key))
+                return layout[key];
+
+            return -1;
         }
     }
 }
