ff0eb321e65341d5bf393ebc24e24062eb01c5f2
Unknown <kamil.pietka@yahoo.pl>
2019-07-14 23:52:54 +0200

    Move ToHsv and FromHsv helpers to ColorUtils

--- Project-Aurora/Project-Aurora/Settings/Layers/RazerLayerHandler.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/RazerLayerHandler.cs
@@ -228,7 +228,7 @@ namespace Aurora.Settings.Layers
             if (color.R == 0 && color.G == 0 && color.B == 0)
                 return color;
 
-            ToHsv(color, out var hue, out var saturation, out var value);
+            ColorUtils.ToHsv(color, out var hue, out var saturation, out var value);
 
             if (Properties.BrightnessBoost > 0)
                 value = Math.Pow(value, 1 - Properties.BrightnessBoost);
@@ -237,54 +237,7 @@ namespace Aurora.Settings.Layers
             if (Properties.HueShift > 0)
                 hue = (hue + Properties.HueShift) % 360;
 
-            return FromHsv(hue, saturation, value);
-        }
-
-        private void ToHsv(Color color, out double hue, out double saturation, out double value)
-        {
-            var max = Math.Max(color.R, Math.Max(color.G, color.B));
-            var min = Math.Min(color.R, Math.Min(color.G, color.B));
-
-            var delta = max - min;
-
-            hue = 0d;
-            if (delta != 0)
-            {
-                if (color.R == max) hue = (color.G - color.B) / (double)delta;
-                else if (color.G == max) hue = 2d + (color.B - color.R) / (double)delta;
-                else if (color.B == max) hue = 4d + (color.R - color.G) / (double)delta;
-            }
-
-            hue *= 60;
-            if (hue < 0.0) hue += 360;
-
-            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
-            value = max / 255d;
-        }
-
-        private Color FromHsv(double hue, double saturation, double value)
-        {
-            saturation = Math.Max(Math.Min(saturation, 1), 0);
-            value = Math.Max(Math.Min(value, 1), 0);
-
-            var hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
-            var f = hue / 60 - Math.Floor(hue / 60);
-
-            value *= 255;
-            var v = (byte)(value);
-            var p = (byte)(value * (1 - saturation));
-            var q = (byte)(value * (1 - f * saturation));
-            var t = (byte)(value * (1 - (1 - f) * saturation));
-
-            switch (hi)
-            {
-                case 0: return Color.FromArgb(v, t, p);
-                case 1: return Color.FromArgb(q, v, p);
-                case 2: return Color.FromArgb(p, v, t);
-                case 3: return Color.FromArgb(p, q, v);
-                case 4: return Color.FromArgb(t, p, v);
-                default: return Color.FromArgb(v, p, q);
-            }
+            return ColorUtils.FromHsv(hue, saturation, value);
         }
 
         public override void Dispose()
--- Project-Aurora/Project-Aurora/Utils/ColorUtils.cs
+++ Project-Aurora/Project-Aurora/Utils/ColorUtils.cs
@@ -296,6 +296,53 @@ namespace Aurora.Utils
             return (color.R << 16) | (color.G << 8) | (color.B);
         }
 
+        public static void ToHsv(Color color, out double hue, out double saturation, out double value)
+        {
+            var max = Math.Max(color.R, Math.Max(color.G, color.B));
+            var min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            var delta = max - min;
+
+            hue = 0d;
+            if (delta != 0)
+            {
+                if (color.R == max) hue = (color.G - color.B) / (double)delta;
+                else if (color.G == max) hue = 2d + (color.B - color.R) / (double)delta;
+                else if (color.B == max) hue = 4d + (color.R - color.G) / (double)delta;
+            }
+
+            hue *= 60;
+            if (hue < 0.0) hue += 360;
+
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max / 255d;
+        }
+
+        public static Color FromHsv(double hue, double saturation, double value)
+        {
+            saturation = Math.Max(Math.Min(saturation, 1), 0);
+            value = Math.Max(Math.Min(value, 1), 0);
+
+            var hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            var f = hue / 60 - Math.Floor(hue / 60);
+
+            value *= 255;
+            var v = (byte)(value);
+            var p = (byte)(value * (1 - saturation));
+            var q = (byte)(value * (1 - f * saturation));
+            var t = (byte)(value * (1 - (1 - f) * saturation));
+
+            switch (hi)
+            {
+                case 0: return Color.FromArgb(v, t, p);
+                case 1: return Color.FromArgb(q, v, p);
+                case 2: return Color.FromArgb(p, v, t);
+                case 3: return Color.FromArgb(p, q, v);
+                case 4: return Color.FromArgb(t, p, v);
+                default: return Color.FromArgb(v, p, q);
+            }
+        }
+
         /// <summary>
         /// Returns a Luma coefficient for brightness of a color
         /// </summary>
