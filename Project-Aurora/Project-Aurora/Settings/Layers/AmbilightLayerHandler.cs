dec576202fc4c06d7b5031d2183e3792119ca15f
Diogo Trindade <diogotr7@gmail.com>
2019-08-22 19:11:07 +0100

    Fixed missing reference

--- Project-Aurora/Project-Aurora/Settings/Layers/AmbilightLayerHandler.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/AmbilightLayerHandler.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
