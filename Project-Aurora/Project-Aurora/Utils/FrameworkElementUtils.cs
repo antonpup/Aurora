4a44204d13767365240c4bac9d2f9174303893cf
Will Bennion <will.bennion@outlook.com>
2019-09-16 20:25:52 +0100

    Added evaluatable to detect if a process is running.

---
+++ Project-Aurora/Project-Aurora/Utils/FrameworkElementUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Aurora.Utils {
+    public static class FrameworkElementExtensions {
+
+        /// <summary>
+        /// Tiny extension for the FrameworkElement that allows to set a binding on an element and return that element (so it can be chained).
+        /// Used in the TypeControlMap to shorten the code.
+        /// </summary>
+        public static T WithBinding<T>(this T self, DependencyProperty dp, Binding binding, BindingMode? bindingMode = null, IValueConverter converter = null) where T : FrameworkElement {
+            if (bindingMode.HasValue)
+                binding.Mode = bindingMode.Value;
+            if (converter != null)
+                binding.Converter = converter;
+            self.SetBinding(dp, binding);
+            return self;
+        }
+
+        /// <summary>
+        /// Tiny extension for the FrameworkElement that allows to set a binding on an element and return that element (so it can be chained).
+        /// Used in the TypeControlMap to shorten the code.
+        /// </summary>
+        public static T WithBinding<T>(this T self, DependencyProperty dp, object source, string path, BindingMode? bindingMode = null, IValueConverter converter = null) where T : FrameworkElement
+            => self.WithBinding(dp, new Binding(path) { Source = source }, bindingMode, converter);
+    }
+}
