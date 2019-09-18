dc5b27e91deaffd4e661b93de9f16dcdb8d47f82
Will Bennion <will.bennion@sitscloud.co.uk>
2019-09-18 16:51:13 +0100

    Remade device layout as SVG instead. Will make it easier for using SVG device images, implement a better custom zoom/panning functionality etc.

---
+++ Project-Aurora/AuroraUI/wwwroot/js/device-layout.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    window.deviceLayout = {
+
+        /** Initialises the JS logic for the global device layout.
+         * @param {HTMLElement} element The target element to initialise the device layout on.
+         * @param {{invokeMethodAsync(name:string, ...params):void}} deviceLayout A .NET reference object for the layout controller. */
+        init(element, deviceLayout) {
+            deviceLayout.invokeMethodAsync("X", "Client message");
+        }
+    };
+})();
