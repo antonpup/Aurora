b8627413d20650cbb3e4d88d6b7f3f33903be156
Diogo Trindade <diogotr7@gmail.com>
2019-09-18 01:02:42 +0100

    Replaced Score Split layer with overrides

--- Project-Aurora/Project-Aurora/Profiles/RocketLeague/RocketLeagueProfile.cs
+++ Project-Aurora/Project-Aurora/Profiles/RocketLeague/RocketLeagueProfile.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using Aurora.Profiles.RocketLeague.Layers;
+using Aurora.Settings.Overrides.Logic.Builder;
+using Aurora.Settings.Overrides.Logic;
 
 namespace Aurora.Profiles.RocketLeague
 {
@@ -51,7 +53,44 @@ namespace Aurora.Profiles.RocketLeague
                     },
                 }),
                 new Layer("Goal Explosion", new RocketLeagueGoalExplosionLayerHandler()),
-                new Layer("Score Split", new RocketLeagueScoreSplitLayerHandler()),
+                new Layer("Score Split", new PercentLayerHandler()
+                {
+                    Properties = new PercentLayerHandlerProperties()
+                    {
+                        _PercentType = PercentEffectType.Progressive_Gradual,
+                        _Sequence = new KeySequence(new FreeFormObject(0,0,700,250))
+                    }
+                },
+                new OverrideLogicBuilder()
+                    .SetDynamicDouble("_MaxValue", new IfElseNumeric(new BooleanAnd( new List<IEvaluatable<bool>>{//if match is tied 0-0
+                                                                                        new BooleanMathsComparison(new NumberGSINumeric("Match/Blue/Goals"), new NumberConstant(0)),
+                                                                                        new BooleanMathsComparison(new NumberGSINumeric("Match/Orange/Goals"), new NumberConstant(0))
+                                                                                    }
+                                                                     ),
+                                                                     new NumberConstant(2),//then set the max to 2
+                                                                     new NumberMathsOperation(new NumberGSINumeric("Match/Blue/Goals"), MathsOperator.Add, new NumberGSINumeric("Match/Orange/Goals"))
+                                                   )
+                    )
+                    .SetDynamicDouble("_Value", new IfElseNumeric( new BooleanAnd( new List<IEvaluatable<bool>>{//if match is tied 0 - 0
+                                                                                        new BooleanMathsComparison(new NumberGSINumeric("Match/Blue/Goals"), new NumberConstant(0)),
+                                                                                        new BooleanMathsComparison(new NumberGSINumeric("Match/Orange/Goals"), new NumberConstant(0))
+                                                                                   }
+                                                                    ),
+                                                                    new NumberConstant(1),//then set the max to 1, so it is split 50-50
+                                                                    new IfElseNumeric(new BooleanGSINumeric("Player/Team", 0), new NumberGSINumeric("Match/Blue/Goals"), new NumberGSINumeric("Match/Orange/Goals"))
+                                                )
+                    )
+                    .SetDynamicColor("_PrimaryColor", new NumberConstant(1), //friendly team color
+                                                      new IfElseNumeric(new BooleanGSINumeric("Player/Team", 0), new NumberGSINumeric("Match/Blue/Red"), new NumberGSINumeric("Match/Orange/Red")),
+                                                      new IfElseNumeric(new BooleanGSINumeric("Player/Team", 0), new NumberGSINumeric("Match/Blue/Green"), new NumberGSINumeric("Match/Orange/Green")),
+                                                      new IfElseNumeric(new BooleanGSINumeric("Player/Team", 0), new NumberGSINumeric("Match/Blue/Blue"), new NumberGSINumeric("Match/Orange/Blue"))
+                    )
+                    .SetDynamicColor("_SecondaryColor", new NumberConstant(1), //enemy team color
+                                                        new IfElseNumeric(new BooleanGSINumeric("Player/Team", 1), new NumberGSINumeric("Match/Blue/Red"), new NumberGSINumeric("Match/Orange/Red")),
+                                                        new IfElseNumeric(new BooleanGSINumeric("Player/Team", 1), new NumberGSINumeric("Match/Blue/Green"), new NumberGSINumeric("Match/Orange/Green")),
+                                                        new IfElseNumeric(new BooleanGSINumeric("Player/Team", 1), new NumberGSINumeric("Match/Blue/Blue"), new NumberGSINumeric("Match/Orange/Blue"))
+                    )
+                ),
                 new Layer("Background Layer", new SolidFillLayerHandler()
                 {
                     Properties = new SolidFillLayerHandlerProperties
