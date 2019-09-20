339c953eba7d058590b49a60a24c62e81a46728b
Will Bennion <will.bennion@sitscloud.co.uk>
2019-09-20 10:36:35 +0100

    Implemented initial version of device movement. The 'DeviceID' is not unique (unsure if it should be?), but we need to replace the 'data-device-id' attribute with some unique way of identifying the device. Also attempt to male it so the pixel under the mouse appears to remain stationary when the user zooms in or out. Demo (https://jsfiddle.net/Wibble199/e703Lnpr/69/) works but doesn't completely work here (though it is still a slight inprovement so will be kept for now).

--- Project-Aurora/AuroraUI/wwwroot/js/device-layout.js
+++ Project-Aurora/AuroraUI/wwwroot/js/device-layout.js
@@ -2,63 +2,133 @@
     // Some config constants
     const MIN_ZOOM = 0.4;
     const MAX_ZOOM = 3;
-    const ZOOM_DELTA = 0.01;
+    const ZOOM_DELTA = -0.01;
 
     window.auroraUi = window.auroraUi || {};
     window.auroraUi.deviceLayout = {
 
         /** Initialises the JS logic for the global device layout.
-         * @param {SVGSVGElement} element The target element to initialise the device layout on.
+         * @param {SVGSVGElement} svg The target element to initialise the device layout on.
          * @param {{invokeMethodAsync(remoteMethodName:string, ...params):void}} deviceLayout A .NET reference object for the layout controller. */
-        init(element, deviceLayout) {
-//            deviceLayout.invokeMethodAsync("X", "Client message");
+        init(svg, deviceLayout) {
 
             let zoom = 1;
             let panX = 0, panY = 0;
 
             /** @type {"none"|"pan"|"move"} */
-            let mode = "none";
+			let mode = "none";
+			let devId, devEl, devGhost;
+			let devX, devY;
             let mouseLastX, mouseLastY;
 
-            element.addEventListener('mousedown', e => {
-                e.preventDefault();
-                if (e.button == 0 && element.dataset["editmode"] == "True" && e.target != element) {
-                    // TODO: Drag some particular device
-                    // Will need to store which device it is
-                    mode = "move";
-                } else if (e.button == 0) {
-                    mode = "pan";
+            svg.addEventListener('pointerdown', e => {
+				e.preventDefault();
+				svg.setPointerCapture(e.pointerId);
+
+                if (e.isPrimary && svg.dataset["editmode"] == "True" && e.target != svg) {
+					devEl = e.target;
+					while (isNaN(devId = +devEl.dataset["deviceId"]) && devEl != svg) {
+						devEl = devEl.parentElement;
+					}
+					if (!isNaN(devId)) {
+						let match = new RegExp(/translate\((\-?\d+) (\-?\d+)\)/).exec(devEl.getAttribute('transform'));
+						devX = +match[1]; devY = +match[2];
+						devGhost = devEl.cloneNode(true);
+						devGhost.style.pointerEvents = "none";
+						devGhost.style.opacity = 0.4;
+						devGhost.zIndex = 1;
+						devEl.parentElement.appendChild(devGhost);
+						mode = "move";
+						document.body.style.cursor = "move";
+					}
+						
+                } else if (e.isPrimary) {
+					mode = "pan";
+					document.body.style.cursor = "move";
                 }
-                mouseLastX = e.clientX;
-                mouseLastY = e.clientY;
             });
 
-            // Listen on the document for mousemove events so that dragging doesn't abruptly stop if the user leaves the SVG region
-            document.addEventListener('mousemove', e => {
-                if (mode == "pan") {
-                    panX -= mouseLastX - e.clientX;
-                    panY -= mouseLastY - e.clientY;
+            // We listen on the document (instead of the SVG element) for mousemove events so that dragging doesn't abruptly stop if the user leaves the SVG region
+            svg.addEventListener('pointermove', e => {
+				let deltaX = mouseLastX - e.clientX, deltaY = mouseLastY - e.clientY;
+
+				if (mode == "move") {
+					//console.log(devX, devY, devG);
+					devX -= deltaX / zoom;
+					devY -= deltaY / zoom;
+					devGhost.setAttribute('transform', `translate(${devX} ${devY})`);
+
+				} else if (mode == "pan") {
+                    panX -= deltaX;
+                    panY -= deltaY;
                     updateTransform();
                 }
                 mouseLastX = e.clientX;
-                mouseLastY = e.clientY;
+				mouseLastY = e.clientY;
+				
+				let [x, y] = mouseEventToSvgCoords(e);
+				document.getElementById("test").setAttribute("cx", x);
+				document.getElementById("test").setAttribute("cy", y);
             });
 
-            // Listen for mouse up events anywhere on the page
-            document.addEventListener('mouseup', e => {
-                mode = "none";
+            // Listen for mouse up events anywhere on the page, not just if the mouse is over the SVG element
+            svg.addEventListener('pointerup', e => {
+				if (mode == "move") {
+					deviceLayout.invokeMethodAsync("SetDevicePosition", devId, devX, devY);
+					//devEl.setAttribute()
+				}
+				if (devGhost) {
+					devGhost.remove();
+					devGhost = null;
+				}
+
+				svg.releasePointerCapture(e.pointerId);
+				mode = "none";
+				document.body.style.cursor = null;
             });
 
             // Scroll wheel handler
-            element.addEventListener('wheel', e => {
+            svg.addEventListener('wheel', e => {
+                // Get the mouse position, relative to the SVG canvas.
+				// This is the point that should remain stationary, relative to the viewport
+				let [relX, relY] = mouseEventToSvgCoords(e);
+
+                // Calculate new zoom level and a ratio between new and old zoom
+                let oldZoom = zoom;
                 zoom = Math.min(Math.max(zoom + e.deltaY * ZOOM_DELTA, MIN_ZOOM), MAX_ZOOM);
-                // TODO: Make it zoom in at the mouse's position, rather than the SVG's origin
+				let zoomRatio = oldZoom / zoom;
+
+                // Pan the canvas slightly so that the point under the mouse doesn't appear to move relative to the viewport
+                panX += (relX * zoomRatio) - relX;
+                panY += (relY * zoomRatio) - relY;
+                
                 updateTransform();
             });
 
+
             // Update the SVG's transform
             function updateTransform() {
-                element.firstElementChild.setAttribute('transform', `translate(${panX}, ${panY}) scale(${zoom})`);
+				svg.firstElementChild.setAttribute('transform', `translate(${panX}, ${panY}) scale(${zoom})`);
+			}
+			
+
+			/** Converts the position of the mouse during the given event relative to the SVG canvas.
+			 * @param {MouseEvent} e The source mouse event. */
+			function mouseEventToSvgCoords(e) {
+				// Cannot use e.offsetX/e.offsetY because this is relative event.target, which, if the mouse is over a device will be that device, not the SVG
+				let svgPos = svg.getBoundingClientRect();
+				return elementToSvgCoords(e.clientX - svgPos.left, e.clientY - svgPos.top);
+			}
+
+            /** Converts a pair X and Y coordinates that are relative to the target SVG element into coordinates that are relative to the SVG items inside the SVG.
+             * @param {number} elementX The X coordinate relative to the target SVG element.
+             * @param {number} elementY The Y coordinate relative to the target SVG element.
+             * @returns {[number,number]} The [X, Y] coordinates relative to the SVG canvas, taking into account pan and zoom level. */
+            function elementToSvgCoords(elementX, elementY) {
+                return [
+                    (elementX - panX) / zoom,
+                    (elementY - panY) / zoom
+                ];
             }
         }
     };
