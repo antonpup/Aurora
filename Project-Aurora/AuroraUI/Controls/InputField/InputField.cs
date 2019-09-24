6c6cbc4979b1efa5b66a2e905b5f4e5216b0723e
Will Bennion <will.bennion@sitscloud.co.uk>
2019-09-24 16:47:50 +0100

    Added initial prototype of `InputField`. `InputField` is a generic input control that will show relevant HTML elements depending on the type of value it is given. It will detect and use any class that extends ComponentBase and has a InputFieldControlAttribute applied. Currently, only a string editor has been implemented. Further developments for this control include being able to specify 'special' names (e.g. password) and pass metadata (such as min/max value) to the control.

---
+++ Project-Aurora/AuroraUI/Controls/InputField/InputField.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AuroraUI.Controls.InputField {
+
+    /// <summary>
+    /// Represents a control that has a particular (variable) type and presents a relevant control to the user.
+    /// </summary>
+    public sealed class InputField<TValue> : ComponentBase {
+
+        [Parameter] public TValue Value { get; set; }
+        [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
+
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder) {
+            var control = GetControlFor(typeof(TValue));
+            if (control == null)
+                // If no control exists for this datatype, show a warning message
+                builder.AddMarkupContent(0, $"<div class='editor-type-error'>Editor for the data type '{typeof(TValue).Name}' is unavailable.</div>");
+            else
+            {
+                // Otherwise, create markup for this control, adding the relevant Value and ValueChanged attributes
+                builder.OpenComponent(0, control);
+                builder.AddAttribute(1, "Value", Value);
+                builder.AddAttribute(2, "ValueChanged", EventCallback.Factory.Create<TValue>(this, newValue => { Value = newValue; ValueChanged.InvokeAsync(Value); }));
+                builder.CloseComponent();
+            }
+        }
+
+        /// <summary>Gets the editor type that will be used for the given data type.</summary>
+        private static Type GetControlFor(Type dataType, string specialName = "")
+            => availableControls.ContainsKey((dataType, specialName.ToLower())) ? availableControls[(dataType, specialName)] // Try find a matching type and special name
+             : availableControls.ContainsKey((dataType, "")) ? availableControls[(dataType, specialName)] // If not, try find the type with no special name
+             : null; // Found no matching control
+
+        // A list of all available inner editor controls
+        private static Dictionary<(Type dataType, string specialName), Type> availableControls =
+            Aurora.Utils.TypeUtils.GetTypesWithCustomAttribute<InputFieldControlAttribute>()
+                .Where(x => typeof(ComponentBase).IsAssignableFrom(x.Key))
+                .ToDictionary(x => (x.Value.Type, x.Value.SpecialName?.ToLower() ?? ""), x => x.Key);
+    }
+}
---
+++ Project-Aurora/AuroraUI/Controls/InputField/InputFieldControlAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AuroraUI.Controls.InputField {
+
+    /// <summary>
+    /// This attribute can be applied to Blazor controls to make them accessible by the 'Field' control.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class InputFieldControlAttribute : Attribute {
+
+        /// <summary>
+        /// The type of data that this field can edit. The Blazor control must have a `Value` property of this type.
+        /// </summary>
+        public Type Type { get; set; }
+
+        /// <summary>
+        /// If a special name is provided, it will mark that this control is a specialized control for a particular purpose.
+        /// An example of this could be a password input, which takes a string (like a normal text input does) but should not be used
+        /// everywhere where a string is required. Metadata attributes on class properties will state whether that property should
+        /// use field input with a special name.
+        /// </summary>
+        public string SpecialName { get; set; }
+
+
+        public InputFieldControlAttribute(Type type) {
+            Type = type;
+        }
+    }
+}
