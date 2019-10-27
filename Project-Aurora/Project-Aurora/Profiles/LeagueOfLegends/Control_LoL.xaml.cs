64c5ba276311c5535dcf874c744a38469152520b
Diogo Trindade <diogotr7@gmail.com>
2019-10-27 16:02:15 +0000

    LoL patch button now properly shows an error window if it fails
    was failing silently until now

--- Project-Aurora/Project-Aurora/Profiles/LeagueOfLegends/Control_LoL.xaml.cs
+++ Project-Aurora/Project-Aurora/Profiles/LeagueOfLegends/Control_LoL.xaml.cs
@@ -53,36 +53,32 @@ namespace Aurora.Profiles.LeagueOfLegends
 
         private void patch_button_Click(object sender, RoutedEventArgs e)
         {
+            string lolpath;
             try
             {
-                string lolpath = "";
-
-                try
-                {
-                    lolpath = (string)Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Riot Games, Inc\League of Legends", "Location", null);
-                }
-                catch (Exception exc)
-                {
-                    lolpath = "";
-                }
-
-                if (!String.IsNullOrWhiteSpace(lolpath))
-                {
-                    lolpath = Path.Combine(lolpath, "Game");
-                    if (Directory.Exists(lolpath))
-                    {
-                        using (BinaryWriter lightfx_wrapper_86 = new BinaryWriter(new FileStream(System.IO.Path.Combine(lolpath, "LightFX.dll"), FileMode.Create)))
-                        {
-                            lightfx_wrapper_86.Write(Properties.Resources.Aurora_LightFXWrapper86);
-                        }
-                        MessageBox.Show("Aurora Wrapper Patch for LightFX applied to\r\n" + lolpath);
-                    }
-                }                
+                lolpath = (string)Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Riot Games, Inc\League of Legends", "Location", null);
             }
-            catch (Exception exc)
+            catch
             {
-                Global.logger.Error("League of Legends path exception: " + exc);
+                lolpath = String.Empty;
             }
+
+            if (!string.IsNullOrWhiteSpace(lolpath))
+            {
+                lolpath = Path.Combine(lolpath, "Game");
+                if (Directory.Exists(lolpath))
+                {
+                    using (BinaryWriter lightfx_wrapper_86 = new BinaryWriter(new FileStream(Path.Combine(lolpath, "LightFX.dll"), FileMode.Create)))
+                    {
+                        lightfx_wrapper_86.Write(Properties.Resources.Aurora_LightFXWrapper86);
+                    }
+                    MessageBox.Show("Aurora Wrapper Patch for LightFX applied to\r\n" + lolpath);
+                    return;
+                }
+            }
+
+            MessageBox.Show("Couldn't find League of Legends path automatically, please patch manually");
+            return;
         }
 
         private void game_enabled_Checked(object sender, RoutedEventArgs e)
