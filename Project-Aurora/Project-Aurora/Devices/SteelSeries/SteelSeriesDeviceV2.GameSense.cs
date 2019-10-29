97a7ca273068d92789021493bad3ef7acbdd4c06
WildWolf <magnus1997.mho@gmail.com>
2019-10-29 10:11:24 +0100

    Starting to rewrite SteelSeries communication

---
+++ Project-Aurora/Project-Aurora/Devices/SteelSeries/SteelSeriesDeviceV2.GameSense.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Aurora.Devices.SteelSeries
+{
+    public partial class SteelSeriesDeviceV2
+    {
+        private HttpClient client = new HttpClient();
+        private JObject baseObject;
+
+        private void sendLispCode()
+        {
+
+        }
+
+        private void loadCoreProps()
+        {
+            var file = new FileInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "SteelSeries/SteelSeries Engine 3/coreProps.json"));
+            if(!file.Exists)
+                throw new FileNotFoundException($"Core Props file could not be found.");
+
+            var reader = file.OpenText();
+            var coreProps = JObject.Parse(reader.ReadToEnd());
+            reader.Dispose();
+            client.BaseAddress = new Uri(coreProps["address"].ToString());
+            sendLispCode();
+        }
+
+        private void sendJson(string endpoint, object obj)
+        {
+            client.PostAsync(endpoint, new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json")).GetAwaiter().GetResult();
+        }
+    }
+
+    static class GoLisp
+    {
+        public static string Code = @"";
+    }
+}
---
+++ Project-Aurora/Project-Aurora/Devices/SteelSeries/SteelSeriesDeviceV2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Aurora.Settings;
+
+namespace Aurora.Devices.SteelSeries
+{
+    public partial class SteelSeriesDeviceV2 : Device
+    {
+        public VariableRegistry GetRegisteredVariables()
+        {
+            return new VariableRegistry();
+        }
+
+        public string GetDeviceName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDeviceDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDeviceUpdatePerformance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Initialize()
+        {
+            loadCoreProps();
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Reconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsInitialized()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsKeyboardConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPeripheralConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateDevice(Dictionary<DeviceKeys, Color> keyColors, DoWorkEventArgs e, bool forced = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateDevice(DeviceColorComposition colorComposition, DoWorkEventArgs e, bool forced = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
