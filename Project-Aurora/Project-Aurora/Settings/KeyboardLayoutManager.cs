20fbf95bf07dee6dacb36311694aff70eca2e6f8
Sam Poulton <35287819+SamPoulton@users.noreply.github.com>
2019-11-12 21:10:11 +0000

    Support for Logitech G815 and G513 (for real this time)

--- Project-Aurora/Project-Aurora/Settings/KeyboardLayoutManager.cs
+++ Project-Aurora/Project-Aurora/Settings/KeyboardLayoutManager.cs
@@ -698,6 +698,10 @@ namespace Aurora.Settings
                 layoutConfigPath = Path.Combine(layoutsPath, "logitech_g410.json");
 			else if (keyboard_preference == PreferredKeyboard.Logitech_G213)
                     layoutConfigPath = Path.Combine(layoutsPath, "logitech_g213.json");
+			else if (keyboard_preference == PreferredKeyboard.Logitech_G815)
+                layoutConfigPath = Path.Combine(layoutsPath, "logitech_g815.json");
+            else if (keyboard_preference == PreferredKeyboard.Logitech_G513)
+                layoutConfigPath = Path.Combine(layoutsPath, "logitech_g513.json");
             else if (keyboard_preference == PreferredKeyboard.Corsair_K95)
                 layoutConfigPath = Path.Combine(layoutsPath, "corsair_k95.json");
             else if (keyboard_preference == PreferredKeyboard.Corsair_K95_PL)
