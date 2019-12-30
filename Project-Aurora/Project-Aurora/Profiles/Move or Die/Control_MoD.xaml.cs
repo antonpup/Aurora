b7f51a13bbb1a707fa0921883d65316f9a03dc22
Diogo Trindade <diogotr7@gmail.com>
2019-12-30 16:25:25 +0000

    cleaned up move or die patching

--- Project-Aurora/Project-Aurora/Profiles/Move or Die/Control_MoD.xaml.cs
+++ Project-Aurora/Project-Aurora/Profiles/Move or Die/Control_MoD.xaml.cs
@@ -1,4 +1,5 @@
 using Aurora.Settings;
+using Aurora.Utils;
 using System;
 using System.IO;
 using System.Windows;
@@ -46,9 +47,8 @@ namespace Aurora.Profiles.Move_or_Die
         private void patch_button_manually_Click(object sender, RoutedEventArgs e)
         {
             var dialog = new System.Windows.Forms.FolderBrowserDialog();
-            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
 
-            if (result == System.Windows.Forms.DialogResult.OK)
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 this.InstallWrapper(dialog.SelectedPath);
 
@@ -56,85 +56,24 @@ namespace Aurora.Profiles.Move_or_Die
             }
         }
 
-        private int GameID = 323850;
+        private const int GameID = 323850;
 
-        private bool InstallWrapper()
+        private bool InstallWrapper(string installpath = "")
         {
-            string installpath = System.IO.Path.Combine(Utils.SteamUtils.GetGamePath(this.GameID), "Love", "win");
+            if (string.IsNullOrWhiteSpace(installpath))
+                installpath = Path.Combine(SteamUtils.GetGamePath(GameID), "Love", "win");
 
-            if (Directory.Exists(installpath))
-            {
-                using (BinaryWriter razer_wrapper_86 = new BinaryWriter(new FileStream(System.IO.Path.Combine(installpath, "RzChromaSDK.dll"), FileMode.Create)))
-                {
-                    razer_wrapper_86.Write(Properties.Resources.Aurora_RazerLEDWrapper86);
-                }
-
-                using (BinaryWriter razer_wrapper_64 = new BinaryWriter(new FileStream(System.IO.Path.Combine(installpath, "RzChromaSDK64.dll"), FileMode.Create)))
-                {
-                    razer_wrapper_64.Write(Properties.Resources.Aurora_RazerLEDWrapper64);
-                }
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private bool InstallWrapper(string installpath)
-        {
-            if (!String.IsNullOrWhiteSpace(installpath))
-            {
-                using (BinaryWriter razer_wrapper_86 = new BinaryWriter(new FileStream(System.IO.Path.Combine(installpath, "RzChromaSDK.dll"), FileMode.Create)))
-                {
-                    razer_wrapper_86.Write(Properties.Resources.Aurora_RazerLEDWrapper86);
-                }
-
-                using (BinaryWriter razer_wrapper_64 = new BinaryWriter(new FileStream(System.IO.Path.Combine(installpath, "RzChromaSDK64.dll"), FileMode.Create)))
-                {
-                    razer_wrapper_64.Write(Properties.Resources.Aurora_RazerLEDWrapper64);
-                }
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return FileUtils.TryWrite(Path.Combine(installpath, "RzChromaSDK.dll"), Properties.Resources.Aurora_RazerLEDWrapper86) &&
+                FileUtils.TryWrite(Path.Combine(installpath, "RzChromaSDK64.dll"), Properties.Resources.Aurora_RazerLEDWrapper64);
         }
 
         private bool UninstallWrapper()
         {
-            String installpath = Utils.SteamUtils.GetGamePath(this.GameID);
-            if (!String.IsNullOrWhiteSpace(installpath))
-            {
-                string path = System.IO.Path.Combine(installpath, "RzChromaSDK.dll");
-                string path64 = System.IO.Path.Combine(installpath, "RzChromaSDK64.dll");
+            var installpath = SteamUtils.GetGamePath(GameID);
 
-                string enginepath = System.IO.Path.Combine(installpath, "Love", "win", "RzChromaSDK.dll");
-                string enginepath64 = System.IO.Path.Combine(installpath, "Love", "win", "RzChromaSDK64.dll");
-
-                if (File.Exists(path))
-                    File.Delete(path);
-
-                if (File.Exists(path64))
-                    File.Delete(path64);
-
-                if (File.Exists(enginepath))
-                    File.Delete(enginepath);
-
-                if (File.Exists(enginepath64))
-                    File.Delete(enginepath64);
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return FileUtils.TryDelete(Path.Combine(installpath, "Love", "win", "RzChromaSDK.dll")) &&
+                FileUtils.TryDelete(Path.Combine(installpath, "Love", "win", "RzChromaSDK64.dll"));
         }
-
 
         private void game_enabled_Checked(object sender, RoutedEventArgs e)
         {
@@ -147,9 +86,11 @@ namespace Aurora.Profiles.Move_or_Die
 
         private void patch_cue_dll_button_Click(object sender, RoutedEventArgs e)
         {
-            string gamepath = Utils.SteamUtils.GetGamePath(this.GameID);
-            bool sucess = disable(System.IO.Path.Combine(gamepath, "Love", "win", "CUESDK.dll"));
-            bool sucess64 = disable(System.IO.Path.Combine(gamepath, "Love", "win", "CUESDK.x64.dll"));
+            string gamepath = Utils.SteamUtils.GetGamePath(GameID);
+
+            bool sucess = FileUtils.TryDisable(Path.Combine(gamepath, "Love", "win", "CUESDK.dll"));
+            bool sucess64 = FileUtils.TryDisable(Path.Combine(gamepath, "Love", "win", "CUESDK.x64.dll"));
+
             if (sucess && sucess64)
                 MessageBox.Show("Sucesfully disabled MoD Corsair support.");
             else if (sucess || sucess64)
@@ -158,46 +99,18 @@ namespace Aurora.Profiles.Move_or_Die
                 MessageBox.Show("Couldn't disabled MoD Corsair support. Is it already disabled?");
         }
 
-        private bool disable(string file)
-        {
-            if (File.Exists(file))
-            {
-                if (File.Exists(file + ".disabled"))
-                    File.Delete(file + ".disabled");
-
-                File.Move(file, file + ".disabled");
-                return true;
-            }
-            else
-                return false;
-        }
-
         private void unpatch_cue_dll_button_Click(object sender, RoutedEventArgs e)
         {
-            string gamepath = Utils.SteamUtils.GetGamePath(this.GameID);
-            bool sucess = enable(System.IO.Path.Combine(gamepath, "Love", "win", "CUESDK.dll"));
-            bool sucess64 = enable(System.IO.Path.Combine(gamepath, "Love", "win", "CUESDK.x64.dll"));
+            string gamepath = SteamUtils.GetGamePath(GameID);
+            bool sucess = FileUtils.TryEnable(Path.Combine(gamepath, "Love", "win", "CUESDK.dll"));
+            bool sucess64 = FileUtils.TryEnable(Path.Combine(gamepath, "Love", "win", "CUESDK.x64.dll"));
+
             if (sucess && sucess64)
                 MessageBox.Show("Sucesfully re-enabled MoD Corsair support.");
             else if (sucess || sucess64)
                 MessageBox.Show("Error: Partly re-enabled MoD Corsair support. Is it already enabled?");
             else
                 MessageBox.Show("Couldn't re-enabled MoD Corsair support. Is it already enabled?");
-        }
-
-        private bool enable(string file)
-        {
-            if (File.Exists(file))
-                return false;
-
-            string file_d = file + ".disabled";
-            if (File.Exists(file_d))
-            {
-                File.Move(file_d, file);
-                return true;
-            }
-            else
-                return false;
         }
     }
 }
