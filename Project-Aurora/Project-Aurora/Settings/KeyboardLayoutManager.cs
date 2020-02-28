827e4cfd13e689e2edfdf60d6435553fddd09498
Simon <aiqiang.fu@hp.com>
2020-02-28 10:20:59 +0800

    Format code

--- Project-Aurora/Project-Aurora/Settings/KeyboardLayoutManager.cs
+++ Project-Aurora/Project-Aurora/Settings/KeyboardLayoutManager.cs
@@ -744,17 +744,14 @@ namespace Aurora.Settings
                 layoutConfigPath = Path.Combine(layoutsPath, "generic_laptop_numpad.json");
             else if (keyboard_preference == PreferredKeyboard.Drevo_BladeMaster)
                 layoutConfigPath = Path.Combine(layoutsPath, "drevo_blademaster.json");
-
             else if (keyboard_preference == PreferredKeyboard.Wooting_Two)
                 layoutConfigPath = Path.Combine(layoutsPath, "wooting_two.json");
             else if (keyboard_preference == PreferredKeyboard.Ducky_Shine_7)
                 layoutConfigPath = Path.Combine(layoutsPath, "ducky_shine_7.json");
             else if (keyboard_preference == PreferredKeyboard.Ducky_One_2_RGB_TKL)
                 layoutConfigPath = Path.Combine(layoutsPath, "ducky_one_2_rgb_tkl.json");
-
             else if (keyboard_preference == PreferredKeyboard.OMEN_Sequencer)
                 layoutConfigPath = Path.Combine(layoutsPath, "omen_sequencer.json");
-
             else
             {
                 LoadNone();
