21ea7cd35cdb1c5a5b54f3c4246956ac86a86f61
gitmacer <timfirefox@gmail.com>
2020-05-23 17:21:45 +0200

    Update to use AutojsonNode...
    -Changed ProviderNode to default one
    -Remove unnecessarily "duplicated" Nodes.

--- Project-Aurora/Project-Aurora/Profiles/Slime Rancher/GSI/Nodes/LocationNode.cs
+++ Project-Aurora/Project-Aurora/Profiles/Slime Rancher/GSI/Nodes/LocationNode.cs
@@ -6,78 +6,63 @@ using System.Threading.Tasks;
 
 namespace Aurora.Profiles.Slime_Rancher.GSI.Nodes
 {
-    public class LocationNode : Node
+    public enum SRZone
     {
-        public BiomeNode In;
+        None = -1,
+        TheRanch = 0,
+        REEF = 1,
+        QUARRY = 2,
+        MOSS = 3,
+        DESERT = 4,
+        SEA = 5,
+        RUINS = 7,
+        RUINS_TRANSITION = 8,
+        WILDS = 9,
+        OGDEN_RANCH = 10,
+        VALLEY = 11,
+        MOCHI_RANCH = 12,
+        SLIMULATIONS = 13,
+        VIKTOR_LAB = 14
+    }
+
+    public class LocationNode : AutoJsonNode<LocationNode>
+    {
+        public SRZone Location => In.Location; //legacy
+        [AutoJsonPropertyName("zone_name")]
         public string ZoneName;
+        [AutoJsonIgnore]
+        public BiomeNode In; //legacy
 
 
         internal LocationNode(string json) : base(json)
         {
-            In = new BiomeNode(json);
-            ZoneName = GetString("zone_name");
+            In = new BiomeNode(json); //legacy
         }
-
     }
 
-    public class BiomeNode : Node
+    public class BiomeNode : AutoJsonNode<BiomeNode>
     {
-        public bool None;
-        public bool TheRanch;
-        public bool TheDryReef;
-        public bool TheIndigoQuarry;
-        public bool TheMossBlanket;
-        public bool TheGlassDesert;
-        public bool TheSlimeSea;
-        public bool TheAncientRuins;
-        public bool TheAncientRuinsCourtyard;
-        public bool TheWilds;
-        public bool OgdensRetreat;
-        public bool NimbleValley;
-        public bool MochisManor;
-        public bool TheSlimeulation;
-        public bool ViktorsWorkshop;
+        [GameStateIgnore]
+        public SRZone Location => (SRZone)GetInt("zone");
 
-        internal BiomeNode(string json) : base(json)
-        {
-            SRZone Location = (SRZone)GetInt("zone");
+        public bool None => Location == SRZone.None;
+        public bool TheRanch => Location == SRZone.TheRanch;
+        public bool TheDryReef => Location == SRZone.REEF;
+        public bool TheIndigoQuarry => Location == SRZone.QUARRY;
+        public bool TheMossBlanket => Location == SRZone.MOSS;
+        public bool TheGlassDesert => Location == SRZone.DESERT;
+        public bool TheSlimeSea => Location == SRZone.SEA;
+        public bool TheAncientRuins => Location == SRZone.RUINS;
+        public bool TheAncientRuinsCourtyard => Location == SRZone.RUINS_TRANSITION;
+        public bool TheWilds => Location == SRZone.WILDS;
+        public bool OgdensRetreat => Location == SRZone.OGDEN_RANCH;
+        public bool NimbleValley => Location == SRZone.VALLEY;
+        public bool MochisManor => Location == SRZone.MOCHI_RANCH;
+        public bool TheSlimeulation => Location == SRZone.SLIMULATIONS;
+        public bool ViktorsWorkshop => Location == SRZone.VIKTOR_LAB;
 
-            this.None = Location == SRZone.NONE;
-            this.TheRanch = Location == SRZone.RANCH;
-            this.TheDryReef = Location == SRZone.REEF;
-            this.TheIndigoQuarry = Location == SRZone.QUARRY;
-            this.TheMossBlanket = Location == SRZone.MOSS;
-            this.TheGlassDesert = Location == SRZone.DESERT;
-            this.TheSlimeSea = Location == SRZone.SEA;
-            this.TheAncientRuins = Location == SRZone.RUINS;
-            this.TheAncientRuinsCourtyard = Location == SRZone.RUINS_TRANSITION;
-            this.TheWilds = Location == SRZone.WILDS;
-            this.OgdensRetreat = Location == SRZone.OGDEN_RANCH;
-            this.NimbleValley = Location == SRZone.VALLEY;
-            this.MochisManor = Location == SRZone.MOCHI_RANCH;
-            this.TheSlimeulation = Location == SRZone.SLIMULATIONS;
-            this.ViktorsWorkshop = Location == SRZone.VIKTOR_LAB;
-        }
+        internal BiomeNode(string json) : base(json) { }
     }
 
-}
-
-enum SRZone
-{
-    NONE = -1,
-    RANCH = 0,
-    REEF = 1,
-    QUARRY = 2,
-    MOSS = 3,
-    DESERT = 4,
-    SEA = 5,
-    RUINS = 7,
-    RUINS_TRANSITION = 8,
-    WILDS = 9,
-    OGDEN_RANCH = 10,
-    VALLEY = 11,
-    MOCHI_RANCH = 12,
-    SLIMULATIONS = 13,
-    VIKTOR_LAB = 14
 }
 
