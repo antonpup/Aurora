04ff4aa94326e2ad0ae8bb687fdddc8fce3d5f43
gitmacer <timfirefox@gmail.com>
2020-06-28 15:31:45 +0200

    Add GOG Utils

---
+++ Project-Aurora/Project-Aurora/Utils/LauncherUtils/GOGUtils.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aurora.Utils
+{
+    /// <summary>
+    /// A class for handling GOG games
+    /// </summary>
+    public static class GOGUtils
+    {
+        /// <summary>
+        /// Retrieves a path to a specified AppID
+        /// </summary>
+        /// <param name="gameId">The game's AppID</param>
+        /// <returns>Path to the location of AppID's install</returns>
+        public static string GetGamePath(int gameId) => (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\GOG.com\Games\" + gameId, "path", null);
+    }
+}
