c2389ff490a4fc21ee9b316a6671cad29474da2b
Diogo Trindade <diogotr7@gmail.com>
2020-07-23 17:25:33 +0100

    Cleaned up keymap generation

--- Project-Aurora/Project-Aurora/Devices/OpenRGB/OpenRGBDevice.cs
+++ Project-Aurora/Project-Aurora/Devices/OpenRGB/OpenRGBDevice.cs
@@ -25,6 +25,7 @@ namespace Aurora.Devices.OpenRGB
         OpenRGBClient client;
         List<OpenRGBDevice> controllers;
         List<OpenRGBColor[]> colors;
+        Dictionary<DK, int>[] dictionaries;
 
         public string GetDeviceDetails()
         {
@@ -74,6 +75,7 @@ namespace Aurora.Devices.OpenRGB
                 client.Connect();
 
                 var controllerCount = client.GetControllerCount();
+                dictionaries = new Dictionary<DK, int>[controllerCount];
                 controllers = new List<OpenRGBDevice>();
                 colors = new List<OpenRGBColor[]>();
 
@@ -85,6 +87,14 @@ namespace Aurora.Devices.OpenRGB
                     for (var j = 0; j < dev.Colors.Length; j++)
                         array[j] = new OpenRGBColor();
                     colors.Add(array);
+                    dictionaries[i] = new Dictionary<DK, int>();
+                    for (int k = 0; k < dev.Leds.Length; k++)
+                    {
+                        if (OpenRGBKeyNames.TryGetValue(dev.Leds[k].Name, out var dk))
+                        {
+                            dictionaries[i].Add(dk, k);
+                        }
+                    }
                 }
             }
             catch (Exception e)
@@ -151,18 +161,15 @@ namespace Aurora.Devices.OpenRGB
 
             for (var i = 0; i < controllers.Count; i++)
             {
-                switch(controllers[i].Type)
+                switch (controllers[i].Type)
                 {
                     case OpenRGBDeviceType.Keyboard:
-                        for (var j = 0; j < controllers[i].Leds.Count(); j++)
+
+                        foreach (var led in keyColors)
                         {
-                            foreach (var kn in OpenRGBKeyNames)
+                            if (dictionaries[i].TryGetValue(led.Key, out int index))
                             {
-                                if(kn.Value == controllers[i].Leds[j].Name)
-                                {
-                                    keyColors.TryGetValue(kn.Key, out var keycolor);
-                                    colors[i][j] = new OpenRGBColor(keycolor.R, keycolor.G, keycolor.B);
-                                }
+                                colors[i][index] = new OpenRGBColor(led.Value.R, led.Value.G, led.Value.B);
                             }
                         }
                         break;
@@ -203,129 +210,130 @@ namespace Aurora.Devices.OpenRGB
             return update_result;
         }
 
-        private readonly Dictionary<DK, String> OpenRGBKeyNames = new Dictionary<DK, String>()
+        private static readonly Dictionary<string, DK> OpenRGBKeyNames = new Dictionary<string, DK>()
         {
-            { DK.A                 , "Key: A"                   },
-            { DK.B                 , "Key: B"                   },
-            { DK.C                 , "Key: C"                   },
-            { DK.D                 , "Key: D"                   },
-            { DK.E                 , "Key: E"                   },
-            { DK.F                 , "Key: F"                   },
-            { DK.G                 , "Key: G"                   },
-            { DK.H                 , "Key: H"                   },
-            { DK.I                 , "Key: I"                   },
-            { DK.J                 , "Key: J"                   },
-            { DK.K                 , "Key: K"                   },
-            { DK.L                 , "Key: L"                   },
-            { DK.M                 , "Key: M"                   },
-            { DK.N                 , "Key: N"                   },
-            { DK.O                 , "Key: O"                   },
-            { DK.P                 , "Key: P"                   },
-            { DK.Q                 , "Key: Q"                   },
-            { DK.R                 , "Key: R"                   },
-            { DK.S                 , "Key: S"                   },
-            { DK.T                 , "Key: T"                   },
-            { DK.U                 , "Key: U"                   },
-            { DK.V                 , "Key: V"                   },
-            { DK.W                 , "Key: W"                   },
-            { DK.X                 , "Key: X"                   },
-            { DK.Y                 , "Key: Y"                   },
-            { DK.Z                 , "Key: Z"                   },
-            { DK.ONE               , "Key: 1"                   },
-            { DK.TWO               , "Key: 2"                   },
-            { DK.THREE             , "Key: 3"                   },
-            { DK.FOUR              , "Key: 4"                   },
-            { DK.FIVE              , "Key: 5"                   },
-            { DK.SIX               , "Key: 6"                   },
-            { DK.SEVEN             , "Key: 7"                   },
-            { DK.EIGHT             , "Key: 8"                   },
-            { DK.NINE              , "Key: 9"                   },
-            { DK.ZERO              , "Key: 0"                   },
-            { DK.ENTER             , "Key: Enter"               },
-            { DK.ESC               , "Key: Escape"              },
-            { DK.BACKSPACE         , "Key: Backspace"           },
-            { DK.TAB               , "Key: Tab"                 },
-            { DK.SPACE             , "Key: Space"               },
-            { DK.MINUS             , "Key: -"                   },
-            { DK.EQUALS            , "Key: ="                   },
-            { DK.OPEN_BRACKET      , "Key: ["                   },
-            { DK.CLOSE_BRACKET     , "Key: ]"                   },
-            { DK.BACKSLASH         , "Key: \\"                  },
-            { DK.HASHTAG           , "Key: #"                   },
-            { DK.SEMICOLON         , "Key: ;"                   },
-            { DK.APOSTROPHE        , "Key: '"                   },
-            { DK.TILDE             , "Key: `"                   },
-            { DK.COMMA             , "Key: ,"                   },
-            { DK.PERIOD            , "Key: ."                   },
-            { DK.FORWARD_SLASH     , "Key: /"                   },
-            { DK.CAPS_LOCK         , "Key: Caps Lock"           },
-            { DK.F1                , "Key: F1"                  },
-            { DK.F2                , "Key: F2"                  },
-            { DK.F3                , "Key: F3"                  },
-            { DK.F4                , "Key: F4"                  },
-            { DK.F5                , "Key: F5"                  },
-            { DK.F6                , "Key: F6"                  },
-            { DK.F7                , "Key: F7"                  },
-            { DK.F8                , "Key: F8"                  },
-            { DK.F9                , "Key: F9"                  },
-            { DK.F10               , "Key: F10"                 },
-            { DK.F11               , "Key: F11"                 },
-            { DK.F12               , "Key: F12"                 },
-            { DK.PRINT_SCREEN      , "Key: Print Screen"        },
-            { DK.SCROLL_LOCK       , "Key: Scroll Lock"         },
-            { DK.PAUSE_BREAK       , "Key: Pause/Break"         },
-            { DK.INSERT            , "Key: Insert"              },
-            { DK.HOME              , "Key: Home"                },
-            { DK.PAGE_UP           , "Key: Page Up"             },
-            { DK.DELETE            , "Key: Delete"              },
-            { DK.END               , "Key: End"                 },
-            { DK.PAGE_DOWN         , "Key: Page Down"           },
-            { DK.ARROW_RIGHT       , "Key: Right Arrow"         },
-            { DK.ARROW_LEFT        , "Key: Left Arrow"          },
-            { DK.ARROW_DOWN        , "Key: Down Arrow"          },
-            { DK.ARROW_UP          , "Key: Up Arrow"            },
-            { DK.NUM_LOCK          , "Key: Num Lock"            },
-            { DK.NUM_SLASH         , "Key: Number Pad /"        },
-            { DK.NUM_ASTERISK      , "Key: Number Pad *"        },
-            { DK.NUM_MINUS         , "Key: Number Pad -"        },
-            { DK.NUM_PLUS          , "Key: Number Pad +"        },
-            { DK.NUM_ENTER         , "Key: Number Pad Enter"    },
-            { DK.NUM_ONE           , "Key: Number Pad 1"        },
-            { DK.NUM_TWO           , "Key: Number Pad 2"        },
-            { DK.NUM_THREE         , "Key: Number Pad 3"        },
-            { DK.NUM_FOUR          , "Key: Number Pad 4"        },
-            { DK.NUM_FIVE          , "Key: Number Pad 5"        },
-            { DK.NUM_SIX           , "Key: Number Pad 6"        },
-            { DK.NUM_SEVEN         , "Key: Number Pad 7"        },
-            { DK.NUM_EIGHT         , "Key: Number Pad 8"        },
-            { DK.NUM_NINE          , "Key: Number Pad 9"        },
-            { DK.NUM_ZERO          , "Key: Number Pad 0"        },
-            { DK.NUM_PERIOD        , "Key: Number Pad ."        },
-            { DK.LEFT_FN           , "Key: Left Fn"             },
-            { DK.FN_Key            , "Key: Right Fn"            },
-            { DK.BACKSLASH_UK      , "Key: \\ (UK)"             },
-            { DK.APPLICATION_SELECT, "Key: Context"             },
-            { DK.LEFT_CONTROL      , "Key: Left Control"        },
-            { DK.LEFT_SHIFT        , "Key: Left Shift"          },
-            { DK.LEFT_ALT          , "Key: Left Alt"            },
-            { DK.LEFT_WINDOWS      , "Key: Left Windows"        },
-            { DK.RIGHT_CONTROL     , "Key: Right Control"       },
-            { DK.RIGHT_SHIFT       , "Key: Right Shift"         },
-            { DK.RIGHT_ALT         , "Key: Right Alt"           },
-            { DK.RIGHT_WINDOWS     , "Key: Right Windows"       },
-            { DK.MEDIA_NEXT        , "Key: Media Next"          },
-            { DK.MEDIA_PREVIOUS    , "Key: Media Previous"      },
-            { DK.MEDIA_STOP        , "Key: Media Stop"          },
-            { DK.MEDIA_PAUSE       , "Key: Media Pause"         },
-            { DK.MEDIA_PLAY        , "Key: Media Play"          },
-            { DK.MEDIA_PLAY_PAUSE  , "Key: Media Play/Pause"    },
-            { DK.VOLUME_MUTE       , "Key: Media Mute"          },
-            { DK.LOGO              , "Logo"                     },
-            { DK.BRIGHTNESS_SWITCH , "Key: Brightness"          },
-            { DK.G0                , "Key: G0"                  },
-            { DK.G1                , "Key: G1"                  },
-            { DK.G2                , "Key: G2"                  },
-            { DK.G3                , "Key: G3"                  },
+            { "Key: A"                , DK.A                 },
+            { "Key: B"                , DK.B                 },
+            { "Key: C"                , DK.C                 },
+            { "Key: D"                , DK.D                 },
+            { "Key: E"                , DK.E                 },
+            { "Key: F"                , DK.F                 },
+            { "Key: G"                , DK.G                 },
+            { "Key: H"                , DK.H                 },
+            { "Key: I"                , DK.I                 },
+            { "Key: J"                , DK.J                 },
+            { "Key: K"                , DK.K                 },
+            { "Key: L"                , DK.L                 },
+            { "Key: M"                , DK.M                 },
+            { "Key: N"                , DK.N                 },
+            { "Key: O"                , DK.O                 },
+            { "Key: P"                , DK.P                 },
+            { "Key: Q"                , DK.Q                 },
+            { "Key: R"                , DK.R                 },
+            { "Key: S"                , DK.S                 },
+            { "Key: T"                , DK.T                 },
+            { "Key: U"                , DK.U                 },
+            { "Key: V"                , DK.V                 },
+            { "Key: W"                , DK.W                 },
+            { "Key: X"                , DK.X                 },
+            { "Key: Y"                , DK.Y                 },
+            { "Key: Z"                , DK.Z                 },
+            { "Key: 1"                , DK.ONE               },
+            { "Key: 2"                , DK.TWO               },
+            { "Key: 3"                , DK.THREE             },
+            { "Key: 4"                , DK.FOUR              },
+            { "Key: 5"                , DK.FIVE              },
+            { "Key: 6"                , DK.SIX               },
+            { "Key: 7"                , DK.SEVEN             },
+            { "Key: 8"                , DK.EIGHT             },
+            { "Key: 9"                , DK.NINE              },
+            { "Key: 0"                , DK.ZERO              },
+            { "Key: Enter"            , DK.ENTER             },
+            { "Key: Enter (ISO)"      , DK.ENTER             },
+            { "Key: Escape"           , DK.ESC               },
+            { "Key: Backspace"        , DK.BACKSPACE         },
+            { "Key: Tab"              , DK.TAB               },
+            { "Key: Space"            , DK.SPACE             },
+            { "Key: -"                , DK.MINUS             },
+            { "Key: ="                , DK.EQUALS            },
+            { "Key: ["                , DK.OPEN_BRACKET      },
+            { "Key: ]"                , DK.CLOSE_BRACKET     },
+            { "Key: \\ (ANSI)"        , DK.BACKSLASH         },
+            { "Key: #"                , DK.HASHTAG           },
+            { "Key: ;"                , DK.SEMICOLON         },
+            { "Key: '"                , DK.APOSTROPHE        },
+            { "Key: `"                , DK.TILDE             },
+            { "Key: ,"                , DK.COMMA             },
+            { "Key: ."                , DK.PERIOD            },
+            { "Key: /"                , DK.FORWARD_SLASH     },
+            { "Key: Caps Lock"        , DK.CAPS_LOCK         },
+            { "Key: F1"               , DK.F1                },
+            { "Key: F2"               , DK.F2                },
+            { "Key: F3"               , DK.F3                },
+            { "Key: F4"               , DK.F4                },
+            { "Key: F5"               , DK.F5                },
+            { "Key: F6"               , DK.F6                },
+            { "Key: F7"               , DK.F7                },
+            { "Key: F8"               , DK.F8                },
+            { "Key: F9"               , DK.F9                },
+            { "Key: F10"              , DK.F10               },
+            { "Key: F11"              , DK.F11               },
+            { "Key: F12"              , DK.F12               },
+            { "Key: Print Screen"     , DK.PRINT_SCREEN      },
+            { "Key: Scroll Lock"      , DK.SCROLL_LOCK       },
+            { "Key: Pause/Break"      , DK.PAUSE_BREAK       },
+            { "Key: Insert"           , DK.INSERT            },
+            { "Key: Home"             , DK.HOME              },
+            { "Key: Page Up"          , DK.PAGE_UP           },
+            { "Key: Delete"           , DK.DELETE            },
+            { "Key: End"              , DK.END               },
+            { "Key: Page Down"        , DK.PAGE_DOWN         },
+            { "Key: Right Arrow"      , DK.ARROW_RIGHT       },
+            { "Key: Left Arrow"       , DK.ARROW_LEFT        },
+            { "Key: Down Arrow"       , DK.ARROW_DOWN        },
+            { "Key: Up Arrow"         , DK.ARROW_UP          },
+            { "Key: Num Lock"         , DK.NUM_LOCK          },
+            { "Key: Number Pad /"     , DK.NUM_SLASH         },
+            { "Key: Number Pad *"     , DK.NUM_ASTERISK      },
+            { "Key: Number Pad -"     , DK.NUM_MINUS         },
+            { "Key: Number Pad +"     , DK.NUM_PLUS          },
+            { "Key: Number Pad Enter" , DK.NUM_ENTER         },
+            { "Key: Number Pad 1"     , DK.NUM_ONE           },
+            { "Key: Number Pad 2"     , DK.NUM_TWO           },
+            { "Key: Number Pad 3"     , DK.NUM_THREE         },
+            { "Key: Number Pad 4"     , DK.NUM_FOUR          },
+            { "Key: Number Pad 5"     , DK.NUM_FIVE          },
+            { "Key: Number Pad 6"     , DK.NUM_SIX           },
+            { "Key: Number Pad 7"     , DK.NUM_SEVEN         },
+            { "Key: Number Pad 8"     , DK.NUM_EIGHT         },
+            { "Key: Number Pad 9"     , DK.NUM_NINE          },
+            { "Key: Number Pad 0"     , DK.NUM_ZERO          },
+            { "Key: Number Pad ."     , DK.NUM_PERIOD        },
+            { "Key: Left Fn"          , DK.LEFT_FN           },
+            { "Key: Right Fn"         , DK.FN_Key            },
+            { "Key: \\ (ISO)"         , DK.BACKSLASH_UK      },
+            { "Key: Context"          , DK.APPLICATION_SELECT},
+            { "Key: Left Control"     , DK.LEFT_CONTROL      },
+            { "Key: Left Shift"       , DK.LEFT_SHIFT        },
+            { "Key: Left Alt"         , DK.LEFT_ALT          },
+            { "Key: Left Windows"     , DK.LEFT_WINDOWS      },
+            { "Key: Right Control"    , DK.RIGHT_CONTROL     },
+            { "Key: Right Shift"      , DK.RIGHT_SHIFT       },
+            { "Key: Right Alt"        , DK.RIGHT_ALT         },
+            { "Key: Right Windows"    , DK.RIGHT_WINDOWS     },
+            { "Key: Media Next"       , DK.MEDIA_NEXT        },
+            { "Key: Media Previous"   , DK.MEDIA_PREVIOUS    },
+            { "Key: Media Stop"       , DK.MEDIA_STOP        },
+            { "Key: Media Pause"      , DK.MEDIA_PAUSE       },
+            { "Key: Media Play"       , DK.MEDIA_PLAY        },
+            { "Key: Media Play/Pause" , DK.MEDIA_PLAY_PAUSE  },
+            { "Key: Media Mute"       , DK.VOLUME_MUTE       },
+            { "Logo"                  , DK.LOGO              },
+            { "Key: Brightness"       , DK.BRIGHTNESS_SWITCH },
+            { "Key: G0"               , DK.G0                },
+            { "Key: G1"               , DK.G1                },
+            { "Key: G2"               , DK.G2                },
+            { "Key: G3"               , DK.G3                },
         };
     }
 }
