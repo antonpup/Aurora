f2f28a9069c573892be91a5547f3b9a2dfae23e3
Diogo Trindade <diogotr7@gmail.com>
2020-08-14 01:33:03 +0100

    cleaned up the corsair rewrite a bit

--- Project-Aurora/Project-Aurora/Devices/Corsair/CorsairDevice.cs
+++ Project-Aurora/Project-Aurora/Devices/Corsair/CorsairDevice.cs
@@ -1,10 +1,12 @@
 ﻿using Aurora.Settings;
+using Aurora.Utils;
 using CorsairRGB.NET;
 using CorsairRGB.NET.Enums;
 using CorsairRGB.NET.Structures;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using CUESDK = CorsairRGB.NET.CUE;
 
@@ -13,10 +15,11 @@ namespace Aurora.Devices.Corsair
     public class CorsairDevice : DefaultDevice
     {
         public override string DeviceName => "Corsair";
-        protected override string DeviceInfo => ": " + GetSubDeviceDetails();
+        protected override string DeviceInfo => ": " + string.Join(", ", deviceInfos.Select(d => d.Model));
 
         private readonly List<CorsairDeviceInfo> deviceInfos = new List<CorsairDeviceInfo>();
 
+        private readonly List<CorsairLedId> AllCorsairLedIds = EnumUtils.GetEnumValues<CorsairLedId>().ToList();
         public override bool Initialize()
         {
             CUESDK.PerformProtocolHandshake();
@@ -45,88 +48,59 @@ namespace Aurora.Devices.Corsair
             CUESDK.ReleaseControl();
         }
 
-        private bool SetDeviceColors(CorsairDeviceType type, int index, Dictionary<DeviceKeys, Color> keyColors)
-        {
-            List<CorsairLedColor> colors = new List<CorsairLedColor>();
-
-            if (LedMaps.MapsMap.TryGetValue(type, out var dict) && dict.Count != 0)
-            {
-                foreach (var led in keyColors)
-                {
-                    if (dict.TryGetValue(led.Key, out var ledid))
-                    {
-                        colors.Add(new CorsairLedColor()
-                        {
-                            LedId = ledid,
-                            R = led.Value.R,
-                            G = led.Value.G,
-                            B = led.Value.B
-                        });
-                    }
-                }
-            }
-            else
-            {
-                if (keyColors.TryGetValue(DeviceKeys.Peripheral_Logo, out var clr))
-                {
-                    foreach (CorsairLedId led in LedMaps.DIYLeds)
-                    {
-                        colors.Add(new CorsairLedColor()
-                        {
-                            LedId = led,
-                            R = clr.R,
-                            G = clr.G,
-                            B = clr.B
-                        });
-                    }
-                }
-            }
-
-            if (colors.Count == 0)
-                return false;
-
-            return CUESDK.SetDeviceColors(index, colors.ToArray());
-        }
-
-        private string GetSubDeviceDetails()
-        {
-            StringBuilder a = new StringBuilder();
-            for (int i = 0; i < deviceInfos.Count; i++)
-            {
-                a.Append(deviceInfos[i].Model);
-                if (deviceInfos[i].Channels.ChannelsCount != 0)
-                    a.Append(": ");
-
-                for (int j = 0; j < deviceInfos[i].Channels.Channels.Length; j++)
-                {
-                    CorsairChannelInfo channels = deviceInfos[i].Channels.Channels[j];
-                    for (int k = 0; k < channels.Devices.Length; k++)
-                    {
-                        a.Append(channels.Devices[k].Type);
-                        if (k != channels.Devices.Length - 1)
-                            a.Append(", ");
-                    }
-                    if (j != deviceInfos[i].Channels.Channels.Length - 1)
-                        a.Append(", ");
-                }
-                if (i != deviceInfos.Count - 1)
-                    a.Append("; ");
-            }
-            return a.ToString();
-        }
-
         public override bool UpdateDevice(Dictionary<DeviceKeys, Color> keyColors, DoWorkEventArgs e, bool forced = false)
         {
             for (int i = 0; i < deviceInfos.Count; i++)
-                SetDeviceColors(deviceInfos[i].Type, i, keyColors);
+            {
+                var deviceInfo = deviceInfos[i];
+                List<CorsairLedColor> colors = new List<CorsairLedColor>();
+
+                if (LedMaps.MapsMap.TryGetValue(deviceInfo.Type, out var dict) && dict.Count != 0)
+                {
+                    foreach (var led in keyColors)
+                    {
+                        if (dict.TryGetValue(led.Key, out var ledid))
+                        {
+                            colors.Add(new CorsairLedColor()
+                            {
+                                LedId = ledid,
+                                R = led.Value.R,
+                                G = led.Value.G,
+                                B = led.Value.B
+                            });
+                        }
+                    }
+                }
+                else
+                {
+                    if (keyColors.TryGetValue(DeviceKeys.Peripheral_Logo, out var clr))
+                    {
+                        //may be a bit drastic: if we dont have a map, set literally everything to the periph logo color
+                        foreach(var led in AllCorsairLedIds)
+                        {
+                            colors.Add(new CorsairLedColor()
+                            {
+                                LedId = led,
+                                R = clr.R,
+                                G = clr.G,
+                                B = clr.B
+                            });
+                        }
+                    }
+                }
+
+                if (colors.Count == 0)
+                    continue;
+
+                CUESDK.SetDeviceColors(i, colors.ToArray());
+            }
 
             return CUESDK.Update();
         }
 
         protected override void RegisterVariables(VariableRegistry variableRegistry)
         {
-            variableRegistry.Register($"{DeviceName}_exclusive", false);
+            variableRegistry.Register($"{DeviceName}_exclusive", false, "Request exclusive control");
         }
     }
-
 }
