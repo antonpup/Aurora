2e2969787398e0e40556667f4c77625eda65cefd
Aytaç Kayadelen <aytackayadelen@gmail.com>
2022-11-18 22:06:32 +0300

    added missing interface

---
+++ Project-Aurora/Project-Aurora/Modules/IAuroraModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Aurora.Modules;
+
+public interface IAuroraModule : IDisposable
+{
+    public Task InitializeAsync();
+    public Task DisposeAsync();
+}
