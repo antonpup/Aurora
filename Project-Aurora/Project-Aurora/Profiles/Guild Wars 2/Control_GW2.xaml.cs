ad4b77dac081fc4f7d0cce0001329f8c03e85c00
Aytaç Kayadelen <aytackayadelen@gmail.com>
2023-02-23 22:04:03 +0300

    update guild wars 2 description

--- Project-Aurora/Project-Aurora/Profiles/Guild Wars 2/Control_GW2.xaml.cs
+++ Project-Aurora/Project-Aurora/Profiles/Guild Wars 2/Control_GW2.xaml.cs
@@ -1,105 +1,78 @@
-﻿using Aurora.Controls;
-using Aurora.Settings;
-using System;
+﻿using System;
 using System.IO;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
 
-namespace Aurora.Profiles.Guild_Wars_2
+namespace Aurora.Profiles.Guild_Wars_2;
+
+/// <summary>
+/// Interaction logic for Control_GW2.xaml
+/// </summary>
+public partial class Control_GW2
 {
-    /// <summary>
-    /// Interaction logic for Control_GW2.xaml
-    /// </summary>
-    public partial class Control_GW2 : UserControl
+    private readonly Application _profileManager;
+
+    public Control_GW2(Application profile)
     {
-        private Application profile_manager;
+        InitializeComponent();
 
-        public Control_GW2(Application profile)
-        {
-            InitializeComponent();
+        _profileManager = profile;
 
-            profile_manager = profile;
+        SetSettings();
 
-            SetSettings();
+        _profileManager.ProfileChanged += Profile_manager_ProfileChanged;
+    }
 
-            profile_manager.ProfileChanged += Profile_manager_ProfileChanged;
-        }
+    private void Profile_manager_ProfileChanged(object sender, EventArgs e)
+    {
+        SetSettings();
+    }
 
-        private void Profile_manager_ProfileChanged(object sender, EventArgs e)
-        {
-            SetSettings();
-        }
+    private void SetSettings()
+    {
+        game_enabled.IsChecked = _profileManager.Settings.IsEnabled;
+    }
 
-        private void SetSettings()
-        {
-            this.game_enabled.IsChecked = profile_manager.Settings.IsEnabled;
-        }
+    private void patch_64bit_button_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new FolderBrowserDialog();
+        DialogResult result = dialog.ShowDialog();
 
-        private void patch_32bit_button_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new System.Windows.Forms.FolderBrowserDialog();
-            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+        if (result != DialogResult.OK) return;
+        if (InstallWrapper(dialog.SelectedPath))
+            MessageBox.Show("Aurora Wrapper Patch for LightFX applied to\r\n" + dialog.SelectedPath);
+        else
+            MessageBox.Show("Aurora LightFX Wrapper could not be installed.");
+    }
 
-            if (result == System.Windows.Forms.DialogResult.OK)
-            {
-                if (InstallWrapper(dialog.SelectedPath, false))
-                    MessageBox.Show("Aurora Wrapper Patch for LightFX applied to\r\n" + dialog.SelectedPath);
-                else
-                    MessageBox.Show("Aurora LightFX Wrapper could not be installed.");
-            }
-        }
+    private void game_enabled_Checked(object sender, RoutedEventArgs e)
+    {
+        if (!IsLoaded) return;
+        _profileManager.Settings.IsEnabled = game_enabled.IsChecked.HasValue && game_enabled.IsChecked.Value;
+        _profileManager.SaveProfiles();
+    }
 
-        private void patch_64bit_button_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new System.Windows.Forms.FolderBrowserDialog();
-            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+    private void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+    }
 
-            if (result == System.Windows.Forms.DialogResult.OK)
-            {
-                if (InstallWrapper(dialog.SelectedPath, true))
-                    MessageBox.Show("Aurora Wrapper Patch for LightFX applied to\r\n" + dialog.SelectedPath);
-                else
-                    MessageBox.Show("Aurora LightFX Wrapper could not be installed.");
-            }
-        }
+    private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+    {
+    }
 
-        private void game_enabled_Checked(object sender, RoutedEventArgs e)
-        {
-            if (IsLoaded)
-            {
-                profile_manager.Settings.IsEnabled = (this.game_enabled.IsChecked.HasValue) ? this.game_enabled.IsChecked.Value : false;
-                profile_manager.SaveProfiles();
-            }
-        }
+    private bool InstallWrapper(string installPath = "")
+    {
+        if (string.IsNullOrWhiteSpace(installPath)) return false;
+        var path = Path.Combine(installPath, "LightFX.dll");
 
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
-        }
+        if (!File.Exists(path))
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
 
-        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
-        {
-        }
+        using var lightfxWrapper = new BinaryWriter(new FileStream(path, FileMode.Create));
+        lightfxWrapper.Write( Properties.Resources.Aurora_LightFXWrapper64 );
 
-        private bool InstallWrapper(string installpath = "", bool is64bit = false)
-        {
-            if (!String.IsNullOrWhiteSpace(installpath))
-            {
-                string path = System.IO.Path.Combine(installpath, "LightFX.dll");
+        return true;
 
-                if (!File.Exists(path))
-                    Directory.CreateDirectory(System.IO.Path.GetDirectoryName(path));
-
-                using (BinaryWriter lightfx_wrapper = new BinaryWriter(new FileStream(path, FileMode.Create)))
-                {
-                    lightfx_wrapper.Write( (is64bit ? Properties.Resources.Aurora_LightFXWrapper64 : Properties.Resources.Aurora_LightFXWrapper86) );
-                }
-
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
     }
 }
