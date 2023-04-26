2fbc85eb1a288f34d8e554c3131d3e971625f09c
Aytaç Kayadelen <aytackayadelen@gmail.com>
2023-04-26 18:44:25 +0200

    merge fix

--- Project-Aurora/Project-Aurora/Devices/YeeLight/YeeLightConnector.cs
+++ Project-Aurora/Project-Aurora/Devices/YeeLight/YeeLightConnector.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
-using System.Threading.Tasks;
 using YeeLightAPI.YeeLightConstants;
 using YeeLightAPI.YeeLightDeviceLocator;
 
