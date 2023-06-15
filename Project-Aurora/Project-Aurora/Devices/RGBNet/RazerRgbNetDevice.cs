9af1783e214e8a3afcf97397b9d3b8797fcbac0f
Aytaç Kayadelen <aytackayadelen@gmail.com>
2023-06-15 21:04:28 +0200

    added razer and wooting rgb.net implementations

---
+++ Project-Aurora/Project-Aurora/Devices/RGBNet/RazerRgbNetDevice.cs
@@ -0,0 +1,11 @@
+﻿using RGB.NET.Core;
+using RGB.NET.Devices.Razer;
+
+namespace Aurora.Devices.RGBNet;
+
+public class RazerRgbNetDevice : RgbNetDevice
+{
+    protected override IRGBDeviceProvider Provider => RazerDeviceProvider.Instance;
+
+    public override string DeviceName => "Razer (RGB.NET)";
+}
---
+++ Project-Aurora/Project-Aurora/Devices/RGBNet/WootingRgbNetDevice.cs
@@ -0,0 +1,12 @@
+﻿using RGB.NET.Core;
+using RGB.NET.Devices.Razer;
+using RGB.NET.Devices.Wooting;
+
+namespace Aurora.Devices.RGBNet;
+
+public class WootingRgbNetDevice : RgbNetDevice
+{
+    protected override IRGBDeviceProvider Provider => WootingDeviceProvider.Instance;
+
+    public override string DeviceName => "Wooting (RGB.NET)";
+}
