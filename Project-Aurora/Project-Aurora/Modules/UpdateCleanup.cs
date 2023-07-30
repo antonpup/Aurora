14742e8113fc94696e557b3212dc46d2e8460afa
Aytaç Kayadelen <aytackayadelen@gmail.com>
2023-07-30 03:11:24 +0200

    clean piling log files

--- Project-Aurora/Project-Aurora/Modules/UpdateCleanup.cs
+++ Project-Aurora/Project-Aurora/Modules/UpdateCleanup.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Lombok.NET;
@@ -9,11 +10,31 @@ public partial class UpdateCleanup : AuroraModule
 {
     protected override async Task Initialize()
     {
+        CleanOldLogiDll();
+        CleanLogs();
+    }
+
+    private static void CleanOldLogiDll()
+    {
         var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         Directory.SetCurrentDirectory(path);
         var logiDll = Path.Combine(path, "LogitechLed.dll");
         if (File.Exists(logiDll))
             File.Delete(logiDll);
+    }
+
+    private static void CleanLogs()
+    {
+        var logFolder = Path.Combine(Global.AppDataDirectory, "Logs");
+        var files = from file in Directory.EnumerateFiles(logFolder) select file;
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileName(file);
+            if (fileName.StartsWith("2023-"))
+            {
+                File.Delete(file);
+            }
+        }
     }
 
     [Async]
