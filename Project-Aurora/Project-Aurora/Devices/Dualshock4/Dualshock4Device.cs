a5342e18153789714d63ec97c1a2f239a09b917b
Aytaç Kayadelen <aytackayadelen@gmail.com>
2023-08-25 18:19:35 +0200

    misc refactors

--- Project-Aurora/Project-Aurora/Devices/Dualshock4/Dualshock4Device.cs
+++ Project-Aurora/Project-Aurora/Devices/Dualshock4/Dualshock4Device.cs
@@ -8,186 +8,153 @@ using System.Drawing;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
 
-namespace Aurora.Devices.Dualshock
+namespace Aurora.Devices.Dualshock;
+
+internal class DS4Container
 {
-    internal class DS4Container
+    public readonly DS4Device Device;
+    private readonly ConnectionType _connectionType;
+    private readonly Color _restoreColor;
+
+    public int Battery { get; private set; }
+    public double Latency { get; private set; }
+    public bool Charging { get; private set; }
+
+    public Color SendColor;
+    public DS4HapticState State;
+
+    public DS4Container(DS4Device device, Color restoreColor)
     {
-        public readonly DS4Device device;
-        public readonly ConnectionType connectionType;
-        public readonly Color RestoreColor;
-
-        public int Battery { get; private set; }
-        public double Latency { get; private set; }
-        public bool Charging { get; private set; }
-
-        public Color sendColor;
-        public DS4HapticState state;
-
-        public DS4Container(DS4Device _device, Color restoreColor)
-        {
-            device = _device;
-            connectionType = device.getConnectionType();
-            device.Report += OnDeviceReport;
-            device.StartUpdate();
-            RestoreColor = restoreColor;
-        }
-
-        private void OnDeviceReport(object? sender, EventArgs e)
-        {
-            Battery = device.Battery;
-            Latency = device.Latency;
-            Charging = device.Charging;
-
-            if (ColorsEqual(sendColor, state.LightBarColor))
-                return;
-
-            state.LightBarExplicitlyOff = sendColor.R == 0 && sendColor.G == 0 && sendColor.B == 0;
-            state.LightBarColor = new DS4Color(sendColor);
-            device.pushHapticState(state);
-        }
-
-        public void Disconnect(bool stop)
-        {
-            sendColor = RestoreColor;
-            Thread.Sleep(10);//HACK: this is terrible, but it works reliably so i'll leave it like this :(
-            //this is needed so that OnDeviceReport has time to send the color once the device sends a report.
-            device.Report -= OnDeviceReport;
-            if (stop)
-            {
-                device.DisconnectBT();
-                device.DisconnectDongle();
-            }
-            device.StopUpdate();
-        }
-
-        public string GetDeviceDetails()
-        {
-            var connectionString = connectionType switch
-            {
-                ConnectionType.BT => "Bluetooth",
-                ConnectionType.SONYWA => "DS4 Wireless adapter",
-                ConnectionType.USB => "USB",
-                _ => "",
-            };
-
-            return $"over {connectionString} {(Charging ? "⚡" : "")}🔋{Battery}% Latency: {Latency:0.00}ms";
-        }
-
-        private bool ColorsEqual(Color clr, DS4Color ds4clr)
-        {
-            return clr.R == ds4clr.red &&
-                    clr.G == ds4clr.green &&
-                    clr.B == ds4clr.blue;
-        }
+        Device = device;
+        _connectionType = Device.getConnectionType();
+        Device.Report += OnDeviceReport;
+        Device.StartUpdate();
+        _restoreColor = restoreColor;
     }
 
-    public class DualshockDevice : DefaultDevice
+    private void OnDeviceReport(object? sender, EventArgs e)
     {
-        public static DualshockDevice? Instance;
+        Battery = Device.Battery;
+        Latency = Device.Latency;
+        Charging = Device.Charging;
+
+        if (ColorsEqual(SendColor, State.LightBarColor))
+            return;
+
+        State.LightBarExplicitlyOff = SendColor.R == 0 && SendColor.G == 0 && SendColor.B == 0;
+        State.LightBarColor = new DS4Color(SendColor);
+        Device.pushHapticState(State);
+    }
+
+    public void Disconnect(bool stop)
+    {
+        SendColor = _restoreColor;
+        Thread.Sleep(10);//HACK: this is terrible, but it works reliably so i'll leave it like this :(
+        //this is needed so that OnDeviceReport has time to send the color once the device sends a report.
+        Device.Report -= OnDeviceReport;
+        if (stop)
+        {
+            Device.DisconnectBT();
+            Device.DisconnectDongle();
+        }
+        Device.StopUpdate();
+    }
+
+    public string GetDeviceDetails()
+    {
+        var connectionString = _connectionType switch
+        {
+            ConnectionType.BT => "Bluetooth",
+            ConnectionType.SONYWA => "DS4 Wireless adapter",
+            ConnectionType.USB => "USB",
+            _ => "",
+        };
+
+        return $"over {connectionString} {(Charging ? "⚡" : "")}🔋{Battery}% Latency: {Latency:0.00}ms";
+    }
+
+    private bool ColorsEqual(Color clr, DS4Color ds4Clr)
+    {
+        return clr.R == ds4Clr.red &&
+               clr.G == ds4Clr.green &&
+               clr.B == ds4Clr.blue;
+    }
+}
+
+public class DualshockDevice : DefaultDevice
+{
+    public static DualshockDevice? Instance;
         
-        public override string DeviceName => "Sony DualShock 4(PS4)";
+    public override string DeviceName => "Sony DualShock 4(PS4)";
 
-        protected override string DeviceInfo =>
-            string.Join(", ", devices.Select((dev, i) => $"#{i + 1} {dev.GetDeviceDetails()}"));
+    protected override string DeviceInfo =>
+        string.Join(", ", devices.Select((dev, i) => $"#{i + 1} {dev.GetDeviceDetails()}"));
 
-        public int Battery => devices.FirstOrDefault()?.Battery ?? -1;
-        public double Latency => devices.FirstOrDefault()?.Latency ?? -1;
-        public bool Charging => devices.FirstOrDefault()?.Charging ?? false;
+    public int Battery => devices.FirstOrDefault()?.Battery ?? -1;
+    public double Latency => devices.FirstOrDefault()?.Latency ?? -1;
+    public bool Charging => devices.FirstOrDefault()?.Charging ?? false;
 
-        private readonly List<DS4Container> devices = new List<DS4Container>();
-        private DeviceKeys key;
-        private bool isDisconnecting;
+    private readonly List<DS4Container> devices = new();
+    private DeviceKeys _key;
 
-        public DualshockDevice()
+    public DualshockDevice()
+    {
+        Instance = this;
+    }
+
+    protected override Task<bool> DoInitialize()
+    {
+        if (IsInitialized)
+            return Task.FromResult(true);
+
+        _key = Global.Configuration.VarRegistry.GetVariable<DeviceKeys>($"{DeviceName}_devicekey");
+        DS4Devices.findControllers();
+
+        var restore = Global.Configuration.VarRegistry.GetVariable<RealColor>($"{DeviceName}_restore_dualshock").GetDrawingColor();
+
+        foreach (var controller in DS4Devices.getDS4Controllers())
+            devices.Add(new DS4Container(controller, restore));
+
+        return Task.FromResult(IsInitialized = devices.Count > 0);
+    }
+
+    protected override Task Shutdown()
+    {
+        if (!IsInitialized)
         {
-            Instance = this;
-        }
-
-        protected override Task<bool> DoInitialize()
-        {
-            if (IsInitialized)
-                return Task.FromResult(true);
-
-            //dummy call, we just need hidsharp to scan for devices once
-            HidSharp.DeviceList.Local.GetAllDevices();
-            HidSharp.DeviceList.Local.Changed += DeviceListChanged;
-
-            key = Global.Configuration.VarRegistry.GetVariable<DeviceKeys>($"{DeviceName}_devicekey");
-            DS4Devices.findControllers();
-
-            var restore = Global.Configuration.VarRegistry.GetVariable<RealColor>($"{DeviceName}_restore_dualshock").GetDrawingColor();
-
-            foreach (var controller in DS4Devices.getDS4Controllers())
-                devices.Add(new DS4Container(controller, restore));
-
-            return Task.FromResult(IsInitialized = devices.Count > 0);
-        }
-
-        private void DeviceListChanged(object? sender, HidSharp.DeviceListChangedEventArgs e)
-        {
-            if ((!Global.Configuration?.EnabledDevices?.Contains(typeof(DualshockDevice)) ?? false) ||
-                (!Global.Configuration?.VarRegistry?.GetVariable<bool>($"{DeviceName}_auto_init") ?? false))
-            {
-                return;
-            }
-
-            if (isDisconnecting)
-                return;
-
-            LogInfo("Detected device list changed, rescanning for controllers...");
-            DS4Devices.findControllers();
-            if (DS4Devices.getDS4Controllers().Count() != devices.Count)
-                Reset().GetAwaiter().GetResult();
-        }
-
-        protected override Task Shutdown()
-        {
-            if (!IsInitialized)
-            {
-                return Task.CompletedTask;
-            }
-
-            HidSharp.DeviceList.Local.Changed -= DeviceListChanged;
-
-            isDisconnecting = true;
-            foreach (var dev in devices)
-                dev.Disconnect(Global.Configuration.VarRegistry.GetVariable<bool>($"{DeviceName}_disconnect_when_stop"));
-
-            DS4Devices.stopControllers();
-            devices.Clear();
-            IsInitialized = false;
-            isDisconnecting = false;
             return Task.CompletedTask;
         }
 
-        protected override async Task<bool> UpdateDevice(Dictionary<DeviceKeys, Color> keyColors, DoWorkEventArgs e, bool forced = false)
+        foreach (var dev in devices)
+            dev.Disconnect(Global.Configuration.VarRegistry.GetVariable<bool>($"{DeviceName}_disconnect_when_stop"));
+
+        DS4Devices.stopControllers();
+        devices.Clear();
+        IsInitialized = false;
+        return Task.CompletedTask;
+    }
+
+    protected override async Task<bool> UpdateDevice(Dictionary<DeviceKeys, Color> keyColors, DoWorkEventArgs e, bool forced = false)
+    {
+        if (!keyColors.TryGetValue(_key, out var clr)) return false;
+        foreach (var dev in devices)
         {
-            if (keyColors.TryGetValue(key, out var clr))
-            {
-                foreach (var dev in devices)
-                {
-                    dev.sendColor = ColorUtils.CorrectWithAlpha(clr);
-                    if (dev.device.isDisconnectingStatus())
-                    {
-                        await Reset().ConfigureAwait(false);
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-
+            dev.SendColor = ColorUtils.CorrectWithAlpha(clr);
+            if (!dev.Device.isDisconnectingStatus()) continue;
+            await Reset().ConfigureAwait(false);
             return false;
         }
 
-        protected override void RegisterVariables(VariableRegistry variableRegistry)
-        {
-            variableRegistry.Register($"{DeviceName}_restore_dualshock", new RealColor(Color.FromArgb(0, 0, 255)), "Restore Color");
-            variableRegistry.Register($"{DeviceName}_devicekey", DeviceKeys.Peripheral, "Key to Use", DeviceKeys.MOUSEPADLIGHT15, DeviceKeys.Peripheral_Logo);
-            variableRegistry.Register($"{DeviceName}_disconnect_when_stop", false, "Disconnect when Stopping");
-            variableRegistry.Register($"{DeviceName}_auto_init", false, "Initialize automatically when plugged in");
-        }
+        return true;
+
+    }
+
+    protected override void RegisterVariables(VariableRegistry variableRegistry)
+    {
+        variableRegistry.Register($"{DeviceName}_restore_dualshock", new RealColor(Color.FromArgb(0, 0, 255)), "Restore Color");
+        variableRegistry.Register($"{DeviceName}_devicekey", DeviceKeys.Peripheral, "Key to Use", DeviceKeys.MOUSEPADLIGHT15, DeviceKeys.Peripheral_Logo);
+        variableRegistry.Register($"{DeviceName}_disconnect_when_stop", false, "Disconnect when Stopping");
+        variableRegistry.Register($"{DeviceName}_auto_init", false, "Initialize automatically when plugged in");
     }
 }
--- Project-Aurora/Project-Aurora/Settings/Layers/Layer.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/Layer.cs
@@ -7,105 +7,100 @@ using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Controls;
 
-namespace Aurora.Settings.Layers
+namespace Aurora.Settings.Layers;
+
+/// <summary>
+/// A class representing a default settings layer
+/// </summary>
+public class Layer : INotifyPropertyChanged, ICloneable, IDisposable
 {
+    [DoNotNotify, JsonIgnore]
+    public Application? AssociatedApplication { get; private set; }
 
-    /// <summary>
-    /// A class representing a default settings layer
-    /// </summary>
-    public class Layer : INotifyPropertyChanged, ICloneable, IDisposable
-    {
-        [DoNotNotify, JsonIgnore]
-        public Application? AssociatedApplication { get; private set; }
+    public string Name { get; set; } = "New Layer";
 
-        public string Name { get; set; } = "New Layer";
+    [OnChangedMethod(nameof(OnHandlerChanged))]
+    public ILayerHandler Handler { get; set; } = new DefaultLayerHandler();
 
-        [OnChangedMethod(nameof(OnHandlerChanged))]
-        public ILayerHandler? Handler { get; set; } = new DefaultLayerHandler();
+    [JsonIgnore]
+    public UserControl Control => Handler.Control;
 
-        [JsonIgnore]
-        public UserControl Control => Handler.Control;
+    public bool Enabled { get; set; } = true;
 
-        public bool Enabled { get; set; } = true;
+    public Dictionary<string, IOverrideLogic>? OverrideLogic { get; set; }
+    // private void OnOverrideLogicChanged() => // Make the logic collection changed event trigger a property change to ensure it gets saved?
 
-        public Dictionary<string, IOverrideLogic>? OverrideLogic { get; set; }
-        // private void OnOverrideLogicChanged() => // Make the logic collection changed event trigger a property change to ensure it gets saved?
+    #region Constructors
+    public Layer() { }
 
-        #region Constructors
-        public Layer() { }
+    public Layer(string name, ILayerHandler? handler = null) : this() {
+        Name = name;
+        Handler = handler ?? Handler;
+    }
 
-        public Layer(string name, ILayerHandler? handler = null) : this() {
-            Name = name;
-            Handler = handler ?? Handler;
-        }
+    private Layer(string name, ILayerHandler handler, Dictionary<string, IOverrideLogic> overrideLogic) : this(name, handler) {
+        OverrideLogic = overrideLogic;
+    }
 
-        public Layer(string name, ILayerHandler handler, Dictionary<string, IOverrideLogic> overrideLogic) : this(name, handler) {
-            OverrideLogic = overrideLogic;
-        }
+    public Layer(string name, ILayerHandler handler, OverrideLogicBuilder builder) : this(name, handler, builder.Create()) { }
+    #endregion
 
-        public Layer(string name, ILayerHandler handler, OverrideLogicBuilder builder) : this(name, handler, builder.Create()) { }
-        #endregion
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+    private void OnHandlerChanged() {
+        if (AssociatedApplication != null)
+            Handler.SetApplication(AssociatedApplication);
+    }
 
-        private void OnHandlerChanged() {
-            if (AssociatedApplication != null)
-                Handler.SetApplication(AssociatedApplication);
-        }
-
-        public EffectLayer Render(IGameState gs) {
-            Stopwatch timer = Stopwatch.StartNew();
-            if (OverrideLogic != null)
+    public EffectLayer Render(IGameState gs) {
+        if (OverrideLogic != null)
+        {
+            // For every property which has an override logic assigned
+            foreach (var kvp in OverrideLogic)
+                // Set the value of the logic evaluation as the override for this property
             {
-                // For every property which has an override logic assigned
-                foreach (var kvp in OverrideLogic)
-                    // Set the value of the logic evaluation as the override for this property
+                var value = kvp.Value.Evaluate(gs);
+                if (kvp.Value.VarType is { IsEnum: true })
                 {
-                    var value = kvp.Value.Evaluate(gs);
-                    if (kvp.Value.VarType is { IsEnum: true })
-                    {
-                        ((IValueOverridable)Handler.Properties).SetOverride(kvp.Key,
-                            value == null ? null : Enum.ToObject(kvp.Value.VarType, value));
-                    }
-                    else
-                    {
-                        ((IValueOverridable)Handler.Properties).SetOverride(kvp.Key, value);
-                    }
+                    ((IValueOverridable)Handler.Properties).SetOverride(kvp.Key,
+                        value == null ? null : Enum.ToObject(kvp.Value.VarType, value));
+                }
+                else
+                {
+                    ((IValueOverridable)Handler.Properties).SetOverride(kvp.Key, value);
                 }
             }
-
-            EffectLayer effectLayer = ((dynamic)Handler.Properties).Enabled ? Handler.PostRenderFX(Handler.Render(gs)) : EffectLayer.EmptyLayer;
-            timer.Stop();
-            return effectLayer;
         }
 
-        public void SetProfile(Application profile) {
-            AssociatedApplication = profile;
-            Handler?.SetApplication(AssociatedApplication);
-        }
-
-        public object Clone() {
-            var str = JsonConvert.SerializeObject(this, Formatting.None, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, Binder = Utils.JSONUtils.SerializationBinder });
-            return JsonConvert.DeserializeObject(
-                str,
-                GetType(),
-                new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace, TypeNameHandling = TypeNameHandling.All, Binder = Utils.JSONUtils.SerializationBinder }
-            )!;
-        }
-
-        public void SetGameState(IGameState newGameState) => Handler.SetGameState(newGameState);
-        public void Dispose() => Handler?.Dispose();
+        var effectLayer = ((dynamic)Handler.Properties).Enabled ? Handler.PostRenderFX(Handler.Render(gs)) : EffectLayer.EmptyLayer;
+        return effectLayer;
     }
 
-    /// <summary>
-    /// Interface for layers that fire an event when the layer is rendered.<para/>
-    /// To use, the layer handler should call <code>LayerRender?.Invoke(this, layer.GetBitmap());</code> at the end of their <see cref="Layer.Render(IGameState)"/> method.
-    /// </summary>
-    public interface INotifyRender {
-        event EventHandler<Bitmap> LayerRender;
+    public void SetProfile(Application profile) {
+        AssociatedApplication = profile;
+        Handler.SetApplication(AssociatedApplication);
     }
+
+    public object Clone() {
+        var str = JsonConvert.SerializeObject(this, Formatting.None, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All, Binder = Utils.JSONUtils.SerializationBinder });
+        return JsonConvert.DeserializeObject(
+            str,
+            GetType(),
+            new JsonSerializerSettings { ObjectCreationHandling = ObjectCreationHandling.Replace, TypeNameHandling = TypeNameHandling.All, Binder = Utils.JSONUtils.SerializationBinder }
+        )!;
+    }
+
+    public void SetGameState(IGameState newGameState) => Handler.SetGameState(newGameState);
+    public void Dispose() => Handler.Dispose();
+}
+
+/// <summary>
+/// Interface for layers that fire an event when the layer is rendered.<para/>
+/// To use, the layer handler should call <code>LayerRender?.Invoke(this, layer.GetBitmap());</code> at the end of their <see cref="Layer.Render(IGameState)"/> method.
+/// </summary>
+public interface INotifyRender {
+    event EventHandler<Bitmap> LayerRender;
 }
